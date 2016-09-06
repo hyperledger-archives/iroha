ccb1e14a6ce9a6a721023d4e30be287c0008cafb
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-06 13:42:12 +0900

    Update entity message pack

--- core/domain/entity.hpp
+++ core/domain/entity.hpp
@@ -1,13 +1,28 @@
 #ifndef __ENTITY__
 #define __ENTITY__
 
+#include <msgpack.hpp>
 #include <string>
+#include <stdio.h>
+#include <vector>
+
+#include "../crypto/signature.hpp"
 
 class Entity{
   public:
     std::string uuid;
+    std::vector<unsigned char> publicKeyVec;
+    std::vector<unsigned char> privateKeyVec;
+
     unsigned char* publicKey;
     unsigned char* privateKey;
+
+    MSGPACK_DEFINE( uuid, publicKeyVec, privateKeyVec);
+
+    Entity(std::string aUuid){
+      uuid = aUuid;
+
+    }
 };
 
 #endif
