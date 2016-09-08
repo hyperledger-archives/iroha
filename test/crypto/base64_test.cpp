667556e37602d9afd28bb020863a6d8eed275c75
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-08 11:21:22 +0900

    Add base64 unit-test

--- test/crypto/base64_test.cpp
+++ test/crypto/base64_test.cpp
@@ -1,6 +1,6 @@
 
 #include "../../core/crypto/base64.hpp"
-#include "../../core/util/test.hpp"
+#include "../../core/util/testutil.hpp"
 
 #include <iostream>
 #include <string>
@@ -8,9 +8,9 @@
 int main(){
 
   util::test("Encode normal char*", []() -> bool{
-    std::string text = "Hey, I'm mizuki";
+    unsigned char* text = (unsigned char*)"Hey, I'm mizuki";
     std::cout<< base64::encode(text) << std::endl;
-    return equals( base64::decode(base64::encode(text)), text);
+    return util::equals( base64::decode(base64::encode(text)), text);
   });
 
   return util::finish();
