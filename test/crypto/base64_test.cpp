e39258fe5824bde0190553b8e089ba92ab905ec7
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-09 04:44:07 +0900

    Add GoogleTest to cmake

--- test/crypto/base64_test.cpp
+++ test/crypto/base64_test.cpp
@@ -2,16 +2,10 @@
 #include "../../core/crypto/base64.hpp"
 #include "../../core/util/testutil.hpp"
 
-#include <iostream>
-#include <string>
+#include <gtest/gtest.h>
 
-int main(){
-
-  util::test("Encode normal char*", []() -> bool{
+TEST(Base64, EncodeAndDecode){
     unsigned char* text = (unsigned char*)"Hey, I'm mizuki";
-    std::cout<< base64::encode(text) << std::endl;
-    return util::equals( base64::decode(base64::encode(text)), text);
-  });
-
-  return util::finish();
+    ASSERT_EQ( text, base64::decode(base64::encode(text)));
 }
+
