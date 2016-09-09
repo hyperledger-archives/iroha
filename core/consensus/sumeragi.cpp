dbc0fe131fbc359a66a6423010e366a592ac6896
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-09-10 08:59:40 +0900

    conform to style

--- core/consensus/sumeragi.cpp
+++ core/consensus/sumeragi.cpp
@@ -1,59 +1,62 @@
+#include "sumeragi.hpp"
+
 #include "../util/logger.hpp"
 #include "../db/ConsensusRepository.hpp"
 #include "../peer/connection.hpp"
-
 #include "../crypto/hash.hpp"
 
 namespace sumeragi{
 
-  struct Context{
-    int numberOfPeers; //peerの数 // TODO: get this from membership service
-    std::string name; //name Options
+struct Context{
+    int numberOfPeers;  // peerの数 // TODO: get this from membership service
+    std::string name;  // name Options
     bool isLeader;
+    int batchSize;
     int myPeerNumber;
     std::string leaderNumber;
     int timeCounter;
     int peerCounter;
     std::unique_ptr<ConsensusRepository> repository;
     std::unique_ptr<TransactionCache> txCache;
-  };
-  void initialize_sumeragi(int myNumber, int aNumberOfPeer, int leaderNumber){
-      logger("initialize_sumeragi, my number:"+std::to_string( myNumber )+" leader:"+std::to_string(myNumber == leaderNumber)+"");
-      context->myPeerNumber = myNumber;
-      context->numberOfPeers = aNumberOfPeer;
-      context->isLeader = myNumber == leaderNumber;
-      context->leaderNumber = std::to_string(leaderNumber);
-      context->timeCounter = 0;
-      context->peerCounter = 0;
-      context->repository = std::make_unique<ConsensusRepository>();
-      buffer = "";
-  }
+};
 
-  void loopLeader(td::shared_ptr<std::string> tx){
+void initialize_sumeragi(int myNumber, int aNumberOfPeer, int leaderNumber, int batchSize) {
+    logger("initialize_sumeragi, my number:"+std::to_string(myNumber)+" leader:"+std::to_string(myNumber == leaderNumber)+"");
+    context->batchSize = batchSize;
+    context->myPeerNumber = myNumber;
+    context->numberOfPeers = aNumberOfPeer;
+    context->isLeader = myNumber == leaderNumber;
+    context->leaderNumber = std::to_string(leaderNumber);
+    context->timeCounter = 0;
+    context->peerCounter = 0;
+    context->repository = std::make_unique<ConsensusRepository>();
 
-  }
+    buffer = "";
+}
 
-  void loopMember(td::shared_ptr<std::string> tx, const int currLeader){
+void loopLeader(td::shared_ptr<std::string> tx) {
+}
 
-  }
+void loopMember(td::shared_ptr<std::string> tx, int const currLeader) {
+}
 
-  void loop(){
-     logger( "start loop" );
-     int count = 0;
-     while(true){ // TODO: replace with callback function
-
+void loop() {
+    logger("start loop");
+    int count = 0;
+    while (true) {  // TODO(M->M): replace with callback function
         if (context->txCache::hasTx()) {
-            const std::shared_ptr<Transaction> tx = context->repository->popTx();
+            std::shared_ptr<Transaction> const tx = context->repository->popTx();
         }
 
         context->timeCounter++;
 
-        const int currLeader = context->numberOfPeers;
-        if(tx->hash % currLeader == context->myPeerNumber){
-          loopLeader(tx);
-        }else{
-          loopMember(tx, currLeader);
+        int const currLeader = context->numberOfPeers;
+        if (tx->hash % currLeader == context->myPeerNumber) {
+            loopLeader(tx);
+        } else {
+            loopMember(tx, currLeader);
         }
-      }
-  }
-};
+    }
+}
+
+};  // namespace sumeragi
--- core/consensus/sumeragi.hpp
+++ core/consensus/sumeragi.hpp
@@ -1,6 +1,13 @@
+#ifndef CORE_CONSENSUS_SUMERAGI_HPP_
+#define CORE_CONSENSUS_SUMERAGI_HPP_
+
 namespace sumeragi{
-  void initialize_sumeragi(int myNumber,int aNumberOfPeer, int leaderNumber);
+  void initialize_sumeragi(int myNumber, int aNumberOfPeer, int leaderNumber, int batchSize = 1);
   void loop();
+  void loopMember(td::shared_ptr<std::string> tx, int const currLeader);
+  void loopLeader(td::shared_ptr<std::string> tx);
   void command(std::string command,std::vector<std::string> args);
   void execute();
 };
+
+#endif  // CORE_CONSENSUS_SUMERAGI_HPP_
