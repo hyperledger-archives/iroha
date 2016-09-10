3c81f9016115c9004f4a874ac57d7e28f3795223
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-10 17:03:31 +0900

    Add base64 test

--- test/crypto/signature_test.cpp
+++ test/crypto/signature_test.cpp
@@ -2,10 +2,12 @@
 
 #include "../../core/crypto/signature.hpp"
 
+#include <tuple>
+#include <iostream>
 #include <gtest/gtest.h>
 
 TEST(Signature, E){
-
+  auto keyPair = signature::generateKeyPair();
 }
 
 
