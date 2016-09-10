b13f387e358b9684fd0d37ab507f4dbe6bd80b17
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-10 22:09:10 +0900

    Add signature test

--- test/crypto/signature_test.cpp
+++ test/crypto/signature_test.cpp
@@ -1,13 +1,24 @@
 
 
 #include "../../core/crypto/signature.hpp"
+#include "../../core/crypto/base64.hpp"
 
 #include <tuple>
 #include <iostream>
 #include <gtest/gtest.h>
 
+#include <cstring>
+
 TEST(Signature, E){
-  auto keyPair = signature::generateKeyPair();
+  std::tuple<
+    unsigned char*,
+    unsigned char*
+  > keyPair = signature::generateKeyPair();
+  
+  std::cout << strlen((char*)std::get<0>(keyPair)) << std::endl;
+  std::cout << strlen((char*)std::get<1>(keyPair)) << std::endl;
+  std::cout << base64::encode(std::get<0>(keyPair)) << std::endl;
+  std::cout << base64::encode(std::get<1>(keyPair)) << std::endl;
 }
 
 
