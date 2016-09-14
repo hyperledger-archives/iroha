39537f768f411c10913d74a8f518a9406ebdc7f4
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-09-14 22:35:40 +0900

    fix compile errors

--- test/crypto/Base64Test.cpp
+++ test/crypto/Base64Test.cpp
@@ -5,7 +5,7 @@
 
 TEST(Base64, EncodeAndDecodeNormalText){
     unsigned char* text = (unsigned char*)"Hey, I'm mizuki";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeJapaneseTextWithNewLine){
@@ -17,7 +17,7 @@ TEST(Base64, EncodeAndDecodeJapaneseTextWithNewLine){
                                             耶万計不己衣天\
                                             阿佐伎喩女美之\
                                             恵比毛勢須");
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeLineText){
@@ -25,38 +25,38 @@ TEST(Base64, EncodeAndDecodeLineText){
  \n \
  \n \ 
 ";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeJapaneseText){
     unsigned char* text = (unsigned char*)"ソラミツ株式会社以呂波耳本へ止千利奴流乎和加餘多連曽津祢那良牟有為能於久耶万計不己衣天阿佐伎喩女美之恵比毛勢須";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeEmptyText){
     unsigned char* text = (unsigned char*)"";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeSpace){
     unsigned char* text = (unsigned char*)"                        ";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 
 TEST(Base64, EncodeAndDecodeBin){
     unsigned char* text = (unsigned char*)"\xFF\x00\xFF\xFF\xFF";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeLongBin){
     unsigned char* text = (unsigned char*)"\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF\xAF\xFF\x00\xFF\xFF\xFF";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeJapanese){
     unsigned char* text = (unsigned char*)"水樹素子";
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
 
 TEST(Base64, EncodeAndDecodeLongText){
@@ -110,5 +110,5 @@ TEST(Base64, EncodeAndDecodeLongText){
     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-    ASSERT_STREQ( (char*)text, (char*)base64::decode(base64::encode(text)));
+    ASSERT_STREQ( (char*)text, (char*)Base64::decode(Base64::encode(text)));
 }
