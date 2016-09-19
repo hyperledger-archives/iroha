faa580e783f455e6bcd456f73fb2f39f82264bd7
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-20 07:24:46 +0900

    Refactoring java code

--- smartContract/SampleCurrency/Repository.java
+++ smartContract/SampleCurrency/Repository.java
@@ -1,12 +1,9 @@
 
-import java.util.Map;
-
+// no package declaration
 public class Repository {
-
     public static native void save(String key,String value);
     public static native void find(String key);
     public static native void update(String key,String value);
     public static native void remove(String key);
-
 }
 
