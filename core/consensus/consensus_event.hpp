519b4894bc182e0fbbc2a8305c468adde39e2260
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-20 16:11:58 +0900

    Update consensus CMakeLists.txt

--- core/consensus/consensus_event.hpp
+++ core/consensus/consensus_event.hpp
@@ -7,7 +7,7 @@
 namespace ConsensusEvent {
 class ConsensusEvent {
     void addSignature(std::string const signature);
-}   
+};
 };  // namespace ConsensusEvent
 
 #endif  // CORE_CONSENSUS_CONSENSUSEVENT_HPP_
