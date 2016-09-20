3c8121da011ff80cb3fdf6ed8fd2645b8e906e80
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-09-20 13:31:37 +0900

    add checks for tx type

--- core/validation/transaction_validator.cpp
+++ core/validation/transaction_validator.cpp
@@ -1,19 +1,14 @@
 #include "transaction_validator.hpp"
 
+#include "../domain/abstract_transaction.hpp"
 #include "../crypto/signature.hpp"
 
 namespace transaction_validator {
-
-void transactionValidator(AbstractTransaction const tx) {
-    logger("initialize_transactionValidator");
-    //TODO:
-}
-
 bool isValid(Transaction const tx) {
-    return signaturesAreValid(tx); // TODO: add more
+    return signaturesAreValid(tx) && validForType; // TODO: add more tests
 }
 
-bool signaturesAreValid(Transaction const tx) {
+bool signaturesAreValid(AbstractTransaction const tx) {
     bool areAllValid = true;
     for (Signature const sig in tx->signatures) { 
         if (!signature::verify(sig, tx::getRawData(), tx::getPublicKey) {
@@ -21,6 +16,46 @@ bool signaturesAreValid(Transaction const tx) {
         }
     }
     return areAllValid; // TODO: change this!
+}
+
+bool validForType(AbstractTransaction const tx) {
+    if (abstract_transaction::TransactionType::transfer == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::addPeer == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::modifyPeer == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::removePeer == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::signatory == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::signatoryAdd == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::signatoryDelete == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::domainDefinition == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::domainRenewal == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::aliasDefinition == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::aliasRenewal == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::assetDefinition == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::message == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::chaincodeInit == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::chaincodeInvoke == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::chaincodeUpdate == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::chaincodeDestory == tx::getType) {
+
+    } else if (abstract_transaction::TransactionType::interchain == tx::getType) {
+
+    }
 }
 
 };  // namespace transaction_validator
