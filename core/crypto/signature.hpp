b60534656d21bde1ddc3ce42fe20df028ea21063
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-22 19:03:34 +0900

    Update signature

---
+++ core/crypto/signature.hpp
@@ -0,0 +1,46 @@
+#ifndef CORE_CRYPTO_SIGNATURE_HPP_
+#define CORE_CRYPTO_SIGNATURE_HPP_
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#include <ed25519.h>
+
+namespace signature {
+
+  class KeyPair {
+   public:
+    std::vector<unsigned char> publicKey;
+    std::vector<unsigned char> privateKey;
+    KeyPair(
+      std::vector<unsigned char>&& pub,
+      std::vector<unsigned char>&& pri
+    ):
+      publicKey(std::move(pub)),
+      privateKey(std::move(pri))
+    {}
+  };
+
+
+  std::string sign(
+    std::string message,
+    KeyPair  keyPair
+  );
+
+  std::string sign(
+    std::string message,
+    std::string publicKey_b64,
+    std::string privateKey_b64
+  );
+
+  bool verify(
+    const std::string signature_b64,
+    const std::string message,
+    const std::string publicKey_b64);
+
+  KeyPair generateKeyPair();
+
+};  // namespace signature
+
+#endif  // CORE_CRYPTO_SIGNATURE_HPP_
