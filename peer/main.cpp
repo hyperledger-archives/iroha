7d2c3ba5e9c85cf356e872e9b3b52d5a8848732f
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-23 14:35:15 +0900

    Update main function

--- peer/main.cpp
+++ peer/main.cpp
@@ -5,6 +5,11 @@
 #include <atomic>
 
 #include "../core/server/http_server.hpp"
+#include "../core/peer/connection.hpp"
+
+#include "../core/util/yaml_loader.hpp"
+#include "../core/util/logger.hpp"
+#include "../core/peer/connection.hpp"
 
 std::atomic_bool running(true); 
 
@@ -14,14 +19,28 @@ void server(){
 
 void sigIntHandler(int param){
   running = false;
-  //std::cout <<"Halt"<< std::endl;
+  logger::info("main", "will halt");
 }
 
 int main() {
   signal(SIGINT, sigIntHandler);
 
+  if(getenv("IROHA_HOME") == nullptr){
+    std::cout << "You must set IROHA_HOME!" << std::endl;
+    return 1;
+  }
+
   //std::cout<<"Process ID is "<< getpid() << std::endl;
-  //connection::initialize_peer();
+  std::unique_ptr<connection::Config> config;
+  std::unique_ptr<yaml::YamlLoader>   yamlLoader(new yaml::YamlLoader(std::string(getenv("IROHA_HOME"))+"/config/config.yml"));
+
+  config->address = std::move(yamlLoader->get<std::string>("mediaDriver", "address"));
+  config->port    = std::move(yamlLoader->get<std::string>("mediaDriver", "port"));
+  config->name    = std::move(yamlLoader->get<std::string>("mediaDriver", "name"));
+  config->publishStreamId    = yamlLoader->get<int>("mediaDriver", "publishStreamId");
+  config->subscribeStreamId  = yamlLoader->get<int>("mediaDriver", "subscribeStreamId");
+  
+  connection::initialize_peer(std::move(config));
 
   std::thread http_th( server );
 
