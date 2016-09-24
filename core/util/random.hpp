327e03d26f9ab7cd5fbcc1d5cb72d6da4d148982
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-25 01:33:52 +0900

    Update random service

--- core/util/random.hpp
+++ core/util/random.hpp
@@ -1,15 +1,16 @@
 #ifndef __RANDOM_HPP_
 #define __RANDOM_HPP_
 
-namespace random{
+#include <random>
+#include "../crypto/hash.hpp"
 
-  #include <random>
-  #include "../crypto/hash.hpp"
+namespace random_service{
+
   std::string makeRandomHash(){
     std::random_device rd;
     return hash::sha3_256_hex(std::to_string(rd()));  
   }
 
-};  // namespace random
+};  // namespace random_service
 
 #endif
