a5d55f1385b33cee6d41aba8b3fff06afd95f6cc
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-26 05:44:06 +0900

    Add peer service

---
+++ core/service/peer_service.hpp
@@ -0,0 +1,20 @@
+#ifndef __CORE_URL_SERVICE_HPP__
+#define __CORE_URL_SERVICE_HPP__
+
+#include <vector>
+#include <string>
+
+#include "../util/yaml_loader.hpp"
+
+namespace service{
+
+    namespace peer{
+        std::vector<std::string> getPeerList(){
+            std::unique_ptr<yaml::YamlLoader> yamlLoader(new yaml::YamlLoader(std::string(getenv("IROHA_HOME"))+"/config/config.yml"));
+            return std::move(yamlLoader->get<std::vector<std::string> >("peer", "ip"));
+        }
+    }
+
+}
+
+#endif
