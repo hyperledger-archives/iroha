336eb04d515dfc180d6bca13055fa18c39fccca6
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-09-29 18:18:14 +0900

    Add event_repository

---
+++ core/repository/consensus/event_repository.hpp
@@ -0,0 +1,20 @@
+#ifndef CORE_REPOSITORY_UNCONFIRMEDTRANSACTIONREPOSITORY_HPP_
+#define CORE_REPOSITORY_UNCONFIRMEDTRANSACTIONREPOSITORY_HPP_
+
+#include <string>
+#include <msgpack.hpp>
+#include "../../model/transactions/abstract_transaction.hpp"
+
+namespace repository{
+namespace unconfirmed_transaction_repository {
+  bool add(std::string hash, const abstract_transaction::AbstractTransaction& tx);
+  bool update(std::string hash, const abstract_transaction::AbstractTransaction& tx);
+  bool remove(std::string hash);
+  bool empty();
+  std::vector<
+    std::unique_ptr<abstract_transaction::AbstractTransaction>
+  > findAll();
+  std::unique_ptr<abstract_transaction::AbstractTransaction>& find(std::string hash);
+};  // namespace unconfirmed_transaction_repository
+};
+#endif  // CORE_REPOSITORY_UNCONFIRMEDTRANSACTIONREPOSITORY_HPP_
