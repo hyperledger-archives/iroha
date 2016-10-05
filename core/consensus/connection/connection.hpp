402c6e595ef9b27596b15604a3b3c926f8b21786
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-10-05 23:10:00 +0900

    Update conensus hpp

--- core/consensus/connection/connection.hpp
+++ core/consensus/connection/connection.hpp
@@ -12,6 +12,7 @@ void initialize_peer(std::unordered_map<std::string, std::string> config);
 bool sendAll(std::string message);
 bool send(std::string to, std::string message);
 bool receive(std::function<void(std::string from, std::string message)> callback);
+
 };  // end connection
 
 #endif
