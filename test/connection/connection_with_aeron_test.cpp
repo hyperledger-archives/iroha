ba4fb578466b6670ba5062b9a8c7e5a3102e57c0
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-10-18 18:56:32 +0900

    Unuse connection test

--- test/connection/connection_with_aeron_test.cpp
+++ test/connection/connection_with_aeron_test.cpp
@@ -10,17 +10,17 @@ int main(int argc, char* argv[]){
         return 1;
     }
     
-while(1){
-    if(std::string(argv[1]) == "sender"){
-        if(argc != 3){
-            return 1;
+    while(1){
+        if(std::string(argv[1]) == "sender"){
+            if(argc != 3){
+                return 1;
+            }
+            connection::sendAll("Mizuki < nya-n!");
+        }else{
+            connection::receive([](std::string from, std::string message){
+                std::cout <<" receive :" << message <<" from:"<< from << "\n";
+            });
         }
-        connection::sendAll("Mizuki < nya-n!");
-    }else{
-        connection::receive([](std::string from, std::string message){
-            std::cout <<" receive :" << message <<" from:"<< from << "\n";
-        });
     }
-}
     return 0;
 }
