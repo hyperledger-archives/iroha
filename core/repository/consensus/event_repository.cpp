323e8830b2db1816ee46b1bb55e99af7ef5a5adb
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-10-20 20:34:44 +0900

    event cache should be using consensus events

--- core/repository/consensus/event_repository.cpp
+++ core/repository/consensus/event_repository.cpp
@@ -4,38 +4,43 @@
 #include <string>
 #include "../../util/convert_string.hpp"
 #include "../../model/transactions/abstract_transaction.hpp"
+#include "../../consensus/consensus_event.hpp"
 
 namespace repository{
 namespace event {
 
-  bool add(const std::string& hash, std::unique_ptr<abstract_transaction::AbstractTransaction> tx){
-    if(tx->getType() == abstract_transaction::TransactionType::message){
+  bool add(const std::string& hash, std::unique_ptr<consensus_event::ConsensusEvent> tx){
+    if(tx->getType() == consensus_event::ConsensusEvent->tx::message){
        /*
         world_state_repository::add(
             hash,
             convert::to_string<
-              abstract_transaction::AbstractTransaction
+              consensus_event::ConsensusEvent
             >(std::move(tx))
         );
         */
     }
   }
 
-  bool update(const std::string& hash, const abstract_transaction::AbstractTransaction& tx){
+  bool update(const std::string& hash, const consensus_event::ConsensusEvent& consensusEvent){
       
   }
+
   bool remove(const std::string& hash){
 
   }
+
   bool empty(){
 
   }
+
   std::vector<
-    std::unique_ptr<abstract_transaction::AbstractTransaction>
+    std::unique_ptr<consensus_event::ConsensusEvent>
   > findAll(){
 
   }
-  std::unique_ptr<abstract_transaction::AbstractTransaction>& find(std::string hash){
+
+  std::unique_ptr<consensus_event::ConsensusEvent>& find(std::string hash){
 
   }
 };
