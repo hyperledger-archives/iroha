b3fdcc4528ce5a3cce21edbcb12d54f74eb0c049
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-10-23 18:15:15 +0900

    fix compile errors

--- peer/main.cpp
+++ peer/main.cpp
@@ -48,9 +48,9 @@ int main() {
  // std::unique_ptr<yaml::YamlLoader>   yaml(new yaml::YamlLoader(std::string(getenv("IROHA_HOME"))+"/config/config.yml"));
   std::string myPublicKey = "AA"; //yaml->get<std::string>("peer", "publicKey"); 
   std::vector<std::unique_ptr<peer::Node>> peer;
-  peer.push_back(std::unique_ptr<peer::Node>(new peer::Node("1.2.5.6","AAA")));
-  peer.push_back(std::unique_ptr<peer::Node>(new peer::Node("1.2.5.6","AAA")));
-  peer.push_back(std::unique_ptr<peer::Node>(new peer::Node("1.2.5.6","AAA")));
+  peer.push_back(std::make_unique<peer::Node>("1.2.5.6","AAA"));
+  peer.push_back(std::make_unique<peer::Node>("1.2.5.6","BBB"));
+  peer.push_back(std::make_unique<peer::Node>("1.2.5.6","CCC"));
 
  //peer::getPeerList();
   sumeragi::initializeSumeragi(myPublicKey, std::move(peer));
