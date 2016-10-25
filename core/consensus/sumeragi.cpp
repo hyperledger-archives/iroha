3cdae7b736991b6ece7582300da914d72e90d95e
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-10-25 18:45:50 +0900

    fix out of bounds error

--- core/consensus/sumeragi.cpp
+++ core/consensus/sumeragi.cpp
@@ -74,7 +74,10 @@ void initializeSumeragi(
 
     context->numValidatingPeers = context->validatingPeers.size();
     context->maxFaulty = context->numValidatingPeers / 3;  // Default to approx. 1/3 of the network. TODO: make this configurable
-    context->proxyTailNdx = context->maxFaulty*2 + 1;      
+    context->proxyTailNdx = context->maxFaulty*2 + 1;
+    if (context->proxyTailNdx >= context->validatingPeers.size()) {
+        context->proxyTailNdx = context->validatingPeers.size()-1;
+    }
     context->panicCount = 0;
 
     //TODO: move the peer service and ordering code to another place
@@ -90,8 +93,8 @@ void processTransaction(std::unique_ptr<ConsensusEvent> event) {
     }
 
     event->addSignature(signature::sign(event->getHash(), peer::getMyPublicKey(), peer::getPrivateKey()));
-    if (context->validatingPeers[context->proxyTailNdx]->getPublicKey() == peer::getMyPublicKey()) {
-        connection::send(context->validatingPeers[context->proxyTailNdx]->getIP(), event->getHash()); // Think In Process
+    if (context->validatingPeers.at(context->proxyTailNdx)->getPublicKey() == peer::getMyPublicKey()) {
+        connection::send(context->validatingPeers.at(context->proxyTailNdx)->getIP(), event->getHash()); // Think In Process
     } else {
         connection::sendAll(event->getHash()); // Think In Process
     }
