e095d25d47c52c018aaed34acad36b65d21d1e19
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-10-25 16:28:47 +0900

    Update testa

--- test/consensus/sumeragi_test.cpp
+++ test/consensus/sumeragi_test.cpp
@@ -1,11 +1,13 @@
 #include "../../core/consensus/sumeragi.hpp"
+#include "../../core/repository/consensus/event_repository.hpp"
+#include "../../core/model/transactions/transfer_transaction.hpp"
 
 #include <gtest/gtest.h>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <memory>
-
+#include <thread>
 
 /*
  Sample Public key
@@ -17,7 +19,7 @@
  public: fccpkrZyLlxJUQm8RpJXedWVZfbg2Dde0iPphwD+jQ0=
 */
 int main(){
-
+    std::string cmd;
     std::vector<std::unique_ptr<peer::Node>> nodes;
     nodes.push_back(std::make_unique<peer::Node>(
         "45.32.158.71",
@@ -26,7 +28,25 @@ int main(){
     ));
     std::string pubKey = "UKsNazVWDZ1oqNBoFpckirYDvwI/LyL9BqNYXVWCqmw=";
     sumeragi::initializeSumeragi( pubKey, std::move(nodes));
-    sumeragi::loop();
+    std::thread http_th( []() {
+        sumeragi::loop();
+    });
+
+    while(1){
+        std::cin>> cmd;
+        if(cmd == "quit") break;
+        repository::event::add("hash", std::make_unique<consensus_event::ConsensusEvent>(
+            std::make_unique<transaction::TransferTransaction>(
+                "fccpkrZyLlxJUQm8RpJXedWVZfbg2Dde0iPphwD+jQ0=",
+                pubKey,
+                "test",
+                cmd
+            )
+        ));
+
+    }
+
+    http_th.detach();
 
     return 0;
 }
