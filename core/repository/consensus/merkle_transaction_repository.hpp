a729ce71c6c6d8fecd354f715da44b79405aebb8
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-10-27 22:58:03 +0900

    update

--- core/repository/consensus/merkle_transaction_repository.hpp
+++ core/repository/consensus/merkle_transaction_repository.hpp
@@ -1,6 +1,7 @@
 /*
 Copyright Soramitsu Co., Ltd. 2016 All Rights Reserved.
-
+http://soramitsu.co.jp
+ 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
