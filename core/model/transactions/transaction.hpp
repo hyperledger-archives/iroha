3aa78b55636ce4fbcf75d0240380d16854159b91
takemiyamakoto <mtakemiya@g.ecc.u-tokyo.ac.jp>
2016-11-01 17:49:24 +0900

    add namespace

--- core/model/transactions/transaction.hpp
+++ core/model/transactions/transaction.hpp
@@ -11,6 +11,31 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#ifndef CORE_DOMAIN_TRANSACTIONS_TRANSACTION_HPP_
+#define CORE_DOMAIN_TRANSACTIONS_TRANSACTION_HPP_
+
+namespace transaction {
+
+enum class TransactionType {
+    addPeer = 0,
+    modifyPeer = 1,
+    removePeer = 2,
+    transfer = 3,
+    signatory = 4,
+    signatoryAdd = 5,
+    signatoryDelete = 6,
+    domainDefinition = 7,
+    domainRenewal = 8,
+    aliasDefinition = 9,
+    aliasRenewal = 10,
+    assetDefinition = 11,
+    message = 12,
+    chaincodeInit = 13,
+    chaincodeInvoke = 14,
+    chaincodeUpdate  = 15,
+    chaincodeDestroy = 16,
+    interchain = 17
+};
 
 template <class T, typename ...commands>
 class Transaction {
@@ -40,3 +65,7 @@ public:
         command(object);
     }
 };
+
+}  // namespace transaction
+
+//#endif  // CORE_DOMAIN_TRANSACTIONS_TRANSACTION_HPP_
