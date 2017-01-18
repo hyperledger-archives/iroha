f0e733c3760a46e4d4f80f787fe72ad3f596d2e8
Bogdan Vaneev <warchantua@gmail.com>
2017-01-18 18:38:52 +0300

    Fix in dockerfile

--- docker/dev/scripts/build-iroha.sh
+++ docker/dev/scripts/build-iroha.sh
@@ -14,15 +14,24 @@ fi
 
 
 # build dependencies
-(cd $IROHA_HOME/core/vendor/leveldb && make -j4)    || error "Can't build leveldb submodule"
-(cd $IROHA_HOME/core/vendor/ed25519 && make -j4)    || error "Can't build ed25519 submodule"
-(cd $IROHA_HOME/core/vendor/KeccakCodePackage && \
-	make -j4 && \
-	make -j4 generic64/libkeccak.a)                 || error "Can't build KeccakCodePackage submodule"
-(cd $IROHA_HOME/core/infra/crypto && make -j4)      || error "Can't build crypto submodule"
+cd $IROHA_HOME/core/vendor/leveldb 
+make clean 2>/dev/null 1>&2
+make -j4 || error "Can't build leveldb submodule"
+
+cd $IROHA_HOME/core/vendor/ed25519
+make clean 2>/dev/null 1>&2
+make -j4 || error "Can't build ed25519 submodule"
+
+cd $IROHA_HOME/core/vendor/KeccakCodePackage
+make clean 2>/dev/null 1>&2
+(make -j4 && make -j4 generic64/libkeccak.a) || error "Can't build KeccakCodePackage submodule"
+
+cd $IROHA_HOME/core/infra/crypto 
+make clean 2>/dev/null 1>&2
+make -j4 || error "Can't build crypto submodule"
 
 # build iroha (important: build using single thread!)
-(mkdir -p $IROHA_BUILD || echo "[+] build directory already exists, rebuilding") && \
-cd $IROHA_BUILD                                                                  && \
-cmake $IROHA_HOME                                                                && \
+mkdir -p $IROHA_BUILD && \
+cd $IROHA_BUILD && \
+cmake $IROHA_HOME && \
 make || error "Can't build iroha"
