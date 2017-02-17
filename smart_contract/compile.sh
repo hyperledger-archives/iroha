a028bfc82a9dd3ae1525d05b3f85429d67ea11e6
motxx <motohiko.ave@gmail.com>
2017-02-17 08:07:26 +0000

    Update compile.sh

--- smart_contract/compile.sh
+++ smart_contract/compile.sh
@@ -1,6 +1,6 @@
 javac -encoding UTF-8 \
-  $IROHA_HOME/smart_contract/test/repository/DomainRepository.java \
-  $IROHA_HOME/smart_contract/test/Test.java
+  $IROHA_HOME/smart_contract/Test/repository/DomainRepository.java \
+  $IROHA_HOME/smart_contract/Test/Test.java
 
 #javah -cp $IROHA_HOME/smart_contract \
 #  -d test/repository/ \
