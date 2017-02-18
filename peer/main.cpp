ee50be73886ce716e0c31dced98278058bb00264
mizuki.sonoko <mizuki.sonoko@gmail.com>
2017-02-18 23:46:03 +0900

    Remove model object in service

--- peer/main.cpp
+++ peer/main.cpp
@@ -31,15 +31,6 @@ limitations under the License.
 
 std::atomic_bool running(true);
 
-template<typename T>
-using Transaction = transaction::Transaction<T>;
-template<typename T>
-using ConsensusEvent = event::ConsensusEvent<T>;
-template<typename T>
-using Add = command::Add<T>;
-template<typename T>
-using Transfer = command::Transfer<T>;
-
 void server(){
     http::server();
 }
