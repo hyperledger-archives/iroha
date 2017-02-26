da8eccc5469bbad8cc1db6f2ceae23f2783a3184
Bogdan Vaneev <warchantua@gmail.com>
2017-02-26 22:22:29 +0300

    more dependencies; but link error; working on fix

--- core/infra/crypto/base64.cpp
+++ core/infra/crypto/base64.cpp
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#include "../../crypto/base64.hpp"
+#include <crypto/base64.hpp>
 
 namespace base64 {
 
--- core/infra/crypto/hash.cpp
+++ core/infra/crypto/hash.cpp
@@ -15,7 +15,6 @@ limitations under the License.
 */
 
 #include <crypto/hash.hpp>
-
 #include <SimpleFIPS202.h>
 #include <string>
 
