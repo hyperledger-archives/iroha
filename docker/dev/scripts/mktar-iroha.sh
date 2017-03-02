75773e187faf6d95d8a451f90f0064e40814bc10
motxx <motohiko.ave@gmail.com>
2017-03-02 14:27:49 +0000

    Fix mktar-iroha.sh based on develop

--- docker/dev/scripts/mktar-iroha.sh
+++ docker/dev/scripts/mktar-iroha.sh
@@ -5,8 +5,19 @@ if [ -z ${IROHA_RELEASE} ]; then
   exit 1
 fi
 
-cd ${IROHA_RELEASE} || (echo "[-] No folder ${IROHA_RELEASE}" && exit 1)
+if [ -z ${IROHA_BUILD} ]; then
+  echo "[FATAL][mktar-iroha.sh] Empty variable IROHA_BUILD"
+  exit 1
+fi
 
+
+if [ -z ${IROHA_HOME} ]; then
+  echo "[FATAL][mktar-iroha.sh] Empty variable IROHA_HOME"
+  exit 1
+fi
+
+mkdir -p $IROHA_RELEASE
+cd ${IROHA_RELEASE}
 rsync -av ${IROHA_BUILD}/bin $IROHA_RELEASE && \
 rsync -av ${IROHA_BUILD}/lib $IROHA_RELEASE && \
 rsync -av ${IROHA_BUILD}/my_test_bin $IROHA_RELEASE && \
