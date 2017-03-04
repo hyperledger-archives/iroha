27bd2fbc7d99ac6f69665fdbe36d017ea9ac83c4
Kitsu <mail@kitsu.me>
2017-03-04 18:05:35 +0300

    Test for consensus validator

--- test/validation/transaction_validator_test.cpp
+++ test/validation/transaction_validator_test.cpp
@@ -23,12 +23,13 @@
 
 using Api::ConsensusEvent;
 using Api::Transaction;
+using Api::EventSignature;
 using Api::TxSignatures;
 
 
 std::string public_key_b64  = "slyr7oz2+EU6dh2dY9+jNeO/hVrXCkT3rGhcNZo5rrE=";
-std::string signature_b64   = "gdMUgjyo++4QpF1xDJNdk1a5zmDAEPM67WD4cn6CVZqDxC8nShb/L1Tokgo53HSOPDB0qXAVzcBvfcJ1WLjrAQ==";
 std::string hash = "46ed8c250356759f68930a94996faaa8f8c98ecbe0dcc58c479c8fad71e30096";
+std::string signature_b64   = "gdMUgjyo++4QpF1xDJNdk1a5zmDAEPM67WD4cn6CVZqDxC8nShb/L1Tokgo53HSOPDB0qXAVzcBvfcJ1WLjrAQ==";
 
 TEST(transaction_validator, verify_transaction_event) {
     auto tx = std::make_unique<Transaction>();
@@ -39,4 +40,27 @@ TEST(transaction_validator, verify_transaction_event) {
     tx->add_txsignatures()->CopyFrom(sig);
     ASSERT_EQ(transaction_validator::signaturesAreValid(tx),
         signature::verify(tx->txsignatures(0).signature(), tx->hash(), tx->txsignatures(0).publickey()));
+
+    // Wrong hashes shouldn't be validated
+    tx->set_hash("123");
+    ASSERT_NE(transaction_validator::signaturesAreValid(tx),
+        signature::verify(signature_b64, hash, public_key_b64));
+}
+
+TEST(transaction_validator, verify_consensus_event) {
+    auto ev = std::make_unique<ConsensusEvent>();
+    auto tx = new Transaction;
+    EventSignature sig;
+    tx->set_hash(hash);
+    sig.set_publickey(public_key_b64);
+    sig.set_signature(signature_b64);
+    ev->set_allocated_transaction(tx);
+    ev->add_eventsignatures()->CopyFrom(sig);
+    ASSERT_EQ(transaction_validator::signaturesAreValid(ev),
+        signature::verify(ev->eventsignatures(0).signature(), ev->transaction().hash(), ev->eventsignatures(0).publickey()));
+
+    // Wrong hashes shouldn't be validated
+    tx->set_hash("123");
+    ASSERT_NE(transaction_validator::signaturesAreValid(ev),
+        signature::verify(signature_b64, hash, public_key_b64));
 }
