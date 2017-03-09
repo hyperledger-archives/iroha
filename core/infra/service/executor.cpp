7d91082c46e28f50435eb3622e160812ff41f046
mizuki.sonoko <mizuki.sonoko@gmail.com>
2017-03-09 13:27:03 +0900

    Add sample command

--- core/infra/service/executor.cpp
+++ core/infra/service/executor.cpp
@@ -19,19 +19,79 @@ namespace executor{
 
     using Api::Transaction;
 
+    void add(const Transaction& tx){
+        if(tx.has_asset()){
+            // Add<Asset>
+        }else if(tx.has_domain()){
+            // Add<Domain>
+        }else if(tx.has_account()){
+            // Add<Account>
+        }else if(tx.has_peer()){
+            // Add<Peer>
+        }
+    }
+
+    void transfer(const Transaction& tx){
+        if(tx.has_asset()){
+            // Transfer<Asset>
+        }else if(tx.has_domain()){
+            // Transfer<Domain>
+        }else if(tx.has_account()){
+            // Transfer<Account>
+        }else if(tx.has_peer()){
+            // Transfer<Peer>
+        }
+    }
+
+    void update(const Transaction& tx){
+        if(tx.has_asset()){
+            // Update<Asset>
+        }else if(tx.has_domain()){
+            // Update<Domain>
+        }else if(tx.has_account()){
+            // Update<Account>
+        }else if(tx.has_peer()){
+            // Update<Peer>
+        }
+    }
+
+    void remove(const Transaction& tx){
+        if(tx.has_asset()){
+            // Remove<Asset>
+        }else if(tx.has_domain()){
+            // Remove<Domain>
+        }else if(tx.has_account()){
+            // Remove<Account>
+        }else if(tx.has_peer()){
+            // Remove<Peer>
+        }
+    }
+
+    void contract(const Transaction& tx){
+        if(tx.has_asset()){
+            // Contract<Asset>
+        }else if(tx.has_domain()){
+            // Contract<Domain>
+        }else if(tx.has_account()){
+            // Contract<Account>
+        }else if(tx.has_peer()){
+            // Contract<Peer>
+        }
+    }
+
     void execute(const Transaction& tx){
         std::cout << "Executor\n";
         std::cout << "DebugString:"<< tx.DebugString() <<"\n";
         if(tx.type() == "add"){
-
+            add(tx);
         }else if(tx.type() == "transfer"){
-
+            transfer(tx);
         }else if(tx.type() == "update"){
-
+            update(tx);
         }else if(tx.type() == "remove"){
-
-        }else if(tx.type() == "transfer"){
-
+            remove(tx);
+        }else if(tx.type() == "contract"){
+            contract(tx);
         }
     }
 
