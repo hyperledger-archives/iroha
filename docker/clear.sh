daf26abacde9115de8f5e20976def76382eb746d
satellitex <s.a.t.e.3.ths@gmail.com>
2017-03-15 03:20:18 +0900

    modify circle.yml add simplei peer-service test / resolve codacy in clear.sh

--- docker/clear.sh
+++ docker/clear.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker rm `docker ps -a -q -f status=exited`
+docker rm $(docker ps -a -q -f status=exited)
 docker rmi -f hyperledger/iroha-docker
 rm -f $IROHA_HOEM/docker/build/iroha.tar
