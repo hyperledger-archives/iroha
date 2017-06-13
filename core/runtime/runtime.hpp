0b387ae0fc5bcfdce828df3ada694a008d14fe02
mizukisonoko <mizuki.sonoko@gmail.com>
2017-06-14 02:08:10 +0900

    Add runtime base

--- core/runtime/runtime.hpp
+++ core/runtime/runtime.hpp
@@ -1,29 +1,15 @@
-/*
-Copyright Soramitsu Co., Ltd. 2016 All Rights Reserved.
+//
+// Created by TaiseiIgarashi on 2017/06/14.
+//
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+#ifndef IROHA_RUNTIME_RUNTIME_HPP
+#define IROHA_RUNTIME_RUNTIME_HPP
 
-                 http://www.apache.org/licenses/LICENSE-2.0
+#include <block.pb.h>
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-#ifndef IROHA_RUNTIME_HPP
-#define IROHA_RUNTIME_HPP
-
-#include <main_generated.h>
-#include "command/add.hpp"
-
-namespace runtime{
-
-    void processTransaction(const iroha::Transaction& tx);
-
-};
+namespace runtime {
+    using Transaction = iroha::protocol::Transaction;
+    void processTransaction(const Transaction&);
+}
 
 #endif //IROHA_RUNTIME_HPP
