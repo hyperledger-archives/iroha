f5986ca8814c1ccc7ff46c96482506fa92f2fc25
satellitex <s.a.t.e.3.ths@gmail.com>
2017-06-15 11:14:12 +0900

    bugfix_sate

--- core/common/random.cpp
+++ core/common/random.cpp
@@ -4,7 +4,7 @@ namespace random_service {
 
 std::string makeHashByMT19937() {
   static std::mt19937 mt(std::random_device{}());
-  return hash::sha3_256_hex(std::to_string(mt()));
+  return crypto::hash::sha3_256_hex(std::to_string(mt()));
 }
 
 };  // namespace random_service
