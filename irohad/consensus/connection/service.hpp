f76b0f7b872ef63649cd93ef0d7a92c683d777b1
motxx <motohiko.ave@gmail.com>
2017-06-16 18:44:56 +0900

    Fix indent

--- irohad/consensus/connection/service.hpp
+++ irohad/consensus/connection/service.hpp
@@ -1,12 +1,9 @@
 /*
 Copyright 2017 Soramitsu Co., Ltd.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,21 +17,21 @@ limitations under the License.
 #include <endpoint.grpc.pb.h>
 #include <endpoint.pb.h>
 
-namespace consensus {
-    namespace connection {
+namespace connection {
+  namespace consensus {
 
-        void receive(const std::function<void(const iroha::protocol::Block&)>&);
+    void receive(const std::function<void(const iroha::protocol::Block&)>&);
 
-        class SumeragiService final
-            : public iroha::protocol::SumeragiService::Service {
-        public:
-            grpc::Status Verify(
-                grpc::ClientContext* context,
-                const iroha::protocol::Block& request,
-                iroha::protocol::VerifyResponse* response);
-        };
+    class SumeragiService final
+      : public iroha::protocol::SumeragiService::Service {
+    public:
+      grpc::Status Verify(
+        grpc::ClientContext* context,
+        const iroha::protocol::Block& request,
+        iroha::protocol::VerifyResponse* response);
+    };
 
-    }  // namespace connection
-}  // namespace consensus
+  }  // namespace consensus
+}  // namespace connection
 
 #endif
