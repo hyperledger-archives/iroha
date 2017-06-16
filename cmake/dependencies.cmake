ad2f6b91e1a974615e16a59735fc92a799e8a0a9
motxx <motohiko.ave@gmail.com>
2017-06-16 22:34:33 +0900

    Use grpc dependencies with same as iroha-ametsuchi

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -147,57 +147,6 @@ set_target_properties(spdlog PROPERTIES
 
 add_dependencies(spdlog gabime_spdlog)
 
-#########################
-#         grpc          #
-#########################
-find_package(grpc)
-
-if (NOT grpc_FOUND)
-  ExternalProject_Add(grpc_grpc
-    GIT_REPOSITORY "https://github.com/grpc/grpc.git"
-    GIT_TAG           "v1.3.0"
-    BUILD_IN_SOURCE   1
-    BUILD_COMMAND     $(MAKE)
-    CONFIGURE_COMMAND "" # remove configure step
-    INSTALL_COMMAND   "" # remove install step
-    TEST_COMMAND      "" # remove test step
-    UPDATE_COMMAND    "" # remove update step
-    )
-  ExternalProject_Get_Property(grpc_grpc source_dir)
-  set(grpc_INCLUDE_DIR   ${source_dir}/include/grpc)
-  set(grpcpp_INCLUDE_DIR ${source_dir}/include/grpc++)
-  set(grpc_LIB           ${source_dir}/libs/opt/libgrpc.so)
-  set(grpcpp_LIB         ${source_dir}/libs/opt/libgrpc++.so)
-  set(gpr_LIB            ${source_dir}/libs/opt/gpr.so)
-endif ()
-
-# libgrpc
-add_library(grpc SHARED IMPORTED)
-set_target_properties(grpc PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
-  IMPORTED_LOCATION ${grpc_LIB}
-  IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-)
-
-# libgpr
-add_library(gpr SHARED IMPORTED)
-set_target_properties(gpr PROPERTIES
-  IMPORTED_LOCATION ${gpr_LIB}
-  IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-)
-
-# libgrpc++
-add_library(grpc++ SHARED IMPORTED)
-set_target_properties(grpc++ PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES ${grpcpp_INCLUDE_DIR}
-  IMPORTED_LOCATION ${grpcpp_LIB}
-  IMPORTED_LINK_INTERFACE_LANGUAGES "CXX"
-)
-
-if(NOT grpc_FOUND)
-  add_dependencies(grpc grpc_grpc)
-endif()
-
 # find_package(libFuzz)
 
 #if (NOT LIBFUZZER_FOUND)
@@ -257,6 +206,65 @@ set_target_properties(protobuf PROPERTIES
 
 if (NOT PROTOBUF_FOUND OR NOT PROTOBUF_PROTOC_EXECUTABLE)
   add_dependencies(protobuf google_protobuf protoc)
+endif ()
+
+#########################
+#         gRPC          #
+#########################
+find_package(GRPC)
+if (NOT GRPC_FOUND)
+  ExternalProject_Add(grpc_grpc
+          GIT_REPOSITORY "https://github.com/grpc/grpc.git"
+          GIT_TAG "v1.3.0"
+          BUILD_IN_SOURCE 1
+          BUILD_COMMAND $(MAKE)
+          CONFIGURE_COMMAND "" # remove configure step
+          INSTALL_COMMAND "" # remove install step
+          TEST_COMMAND "" # remove test step
+          UPDATE_COMMAND "" # remove update step
+          )
+  ExternalProject_Get_Property(grpc_grpc source_dir)
+  set(grpc_INCLUDE_DIR ${source_dir}/include)
+  set(grpc_LIBRARY ${source_dir}/libs/opt/libgrpc.so)
+  set(grpc_grpc++_LIBRARY ${source_dir}/libs/opt/libgrpc++.so)
+  set(grpc_grpc++_reflection_LIBRARY ${source_dir}/libs/opt/libgrpc++_reflection.so)
+  set(grpc_CPP_PLUGIN ${source_dir})
+  file(MAKE_DIRECTORY ${grpc_INCLUDE_DIR})
+  add_custom_target(grpc_cpp_plugin DEPENDS grpc_grpc protoc)
+else ()
+  set(grpc_INCLUDE_DIR ${GRPC_INCLUDE_DIR})
+  set(grpc_LIBRARY ${GRPC_LIBRARY})
+  set(grpc_grpc++_LIBRARY ${GRPC_GRPC++_LIBRARY})
+  set(grpc_grpc++_reflection_LIBRARY ${GRPC_GRPC++_REFLECTION_LIBRARY})
+  set(grpc_CPP_PLUGIN ${GRPC_CPP_PLUGIN})
+  add_custom_target(grpc_cpp_plugin DEPENDS protoc)
+endif ()
+
+# libgrpc
+add_library(grpc SHARED IMPORTED)
+set_target_properties(grpc PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
+        IMPORTED_LOCATION ${grpc_LIBRARY}
+        )
+
+# libgrpc++
+add_library(grpc++ SHARED IMPORTED)
+set_target_properties(grpc++ PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
+        IMPORTED_LOCATION ${grpc_grpc++_LIBRARY}
+        )
+
+# libgrpc++_reflection
+add_library(grpc++_reflection SHARED IMPORTED)
+set_target_properties(grpc++_reflection PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
+        IMPORTED_LOCATION ${grpc_grpc++_reflection_LIBRARY}
+        )
+
+if (NOT GRPC_FOUND)
+  add_dependencies(grpc grpc_grpc)
+  add_dependencies(grpc++ grpc_grpc)
+  add_dependencies(grpc++_reflection grpc_grpc)
 endif ()
 
 
