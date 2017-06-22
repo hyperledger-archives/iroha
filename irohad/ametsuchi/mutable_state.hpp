24d0129d1b971fd389501761c030311c463b453e
Andrei Lebedev <lebdron@gmail.com>
2017-06-22 18:22:06 +0300

    Add Storage and MutableState interfaces

---
+++ irohad/ametsuchi/mutable_state.hpp
@@ -0,0 +1,73 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_MUTABLESTATE_HPP
+#define IROHA_MUTABLESTATE_HPP
+
+#include <ametsuchi/query_api.h>
+#include <dao/block.hpp>
+#include <dao/transaction.hpp>
+
+namespace iroha {
+
+  namespace ametsuchi {
+
+    class StorageException;
+
+    /**
+     * Reflects a temporary state of storage
+     * Allows queries on the temporary state
+     */
+    class MutableState : public QueryApi {
+
+      /**
+       * The context used in @see apply transaction method
+       */
+      class TransactionContext {
+        /**
+         * Attempts to apply a command to current mutable state
+         * Throws exception in case of internal error
+         * @tparam T Command type
+         * @param command Command to be applied
+         * @throws StorageException
+         */
+        template<class T>
+        virtual void try_apply(T command) = 0;
+      };
+
+      /**
+       * Applies a block to current mutable state
+       * @param block Block to be applied
+       */
+      virtual void apply(dao::Block block) = 0;
+
+      /**
+       * Applies a transaction to current mutable state
+       * using logic specified in function
+       * @param transaction Transaction to be applies
+       * @param function Function that specifies the logic used to apply
+       */
+      virtual void apply(dao::Transaction transaction,
+                         std::function<bool(dao::Transaction,
+                                            TransactionContext)> function) = 0;
+    };
+
+  }
+
+} // namespace iroha
+
+#endif //IROHA_MUTABLESTATE_HPP
