7982042e2609f00ad7f0702fb8cf882dffc3bfb5
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-07-02 12:52:25 +0300

    Fix cmake list for processors and torii; add torii stub as with client processor usage

---
+++ irohad/torii/torii_stub.hpp
@@ -0,0 +1,53 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_TORII_STUB_HPP
+#define IROHA_TORII_STUB_HPP
+
+#include <torii/processor/client_processor.hpp>
+#include <dao/dao.hpp>
+
+namespace iroha {
+  namespace torii {
+
+    /**
+     * Stub for torii grpc service;
+     */
+    class ToriiStub {
+     public:
+      explicit ToriiStub(ClientProcessor &processor);
+
+      /**
+       * Emulate query request from client
+       * @param client - query owner
+       * @param query - request for some storage information
+       */
+      void get_query(dao::Client client, dao::Query &query);
+
+      /**
+       * Emulate transaction request from client
+       * @param client - transaction owner
+       * @param tx - intent on changing state of ledger
+       */
+      void get_transaction(dao::Client client, dao::Transaction &tx);
+
+     private:
+      ClientProcessor &processor_;
+    };
+  } // namespace torii
+} // namespace iroha
+#endif //IROHA_TORII_STUB_HPP
