8134e99f6dede26c029666ec6df9b7b441143ddd
kamilsa <kamilsa16@gmail.com>
2017-07-15 11:29:48 +0300

    [WIP] add asset qty serialization

---
+++ irohad/ametsuchi/impl/block_serializer.cpp
@@ -0,0 +1,150 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <ametsuchi/block_serializer.hpp>
+#include <iostream>
+#include <model/commands/add_asset_quantity.hpp>
+#include <model/commands/add_peer.hpp>
+#include <sstream>
+
+namespace iroha {
+  namespace ametsuchi {
+
+    using namespace rapidjson;
+
+    void BlockSerializer::serialize(PrettyWriter<StringBuffer>& writer,
+                                    model::Block block) {
+      writer.StartObject();
+      writer.String("hash");
+      writer.String(block.hash.to_string().c_str());
+
+      writer.String("signatures");
+      writer.StartArray();
+      for (auto sig : block.sigs) {
+        serialize(writer, sig);
+      }
+      writer.EndArray();
+
+      writer.String("created_ts");
+      writer.Uint64(block.created_ts);
+
+      writer.String("height");
+      writer.Uint64(block.height);
+
+      writer.String("prev_hash");
+      writer.String(block.prev_hash.to_string().c_str());
+
+      writer.String("txs_number");
+      writer.Uint(block.txs_number);
+
+      writer.String("merkle_root");
+      writer.String(block.merkle_root.to_string().c_str());
+
+      writer.String("transactions");
+      writer.StartArray();
+      for (auto tx : block.transactions) {
+        serialize(writer, tx);
+      }
+      writer.EndArray();
+
+      writer.EndObject();
+    }
+
+    void BlockSerializer::serialize(PrettyWriter<StringBuffer>& writer,
+                                    model::Signature signature) {
+      writer.StartObject();
+
+      writer.String("pubkey");
+      writer.String(signature.pubkey.to_string().c_str());
+
+      writer.String("signature");
+      writer.String(signature.signature.to_string().c_str());
+
+      writer.EndObject();
+    }
+
+    void BlockSerializer::serialize(PrettyWriter<StringBuffer>& writer,
+                                    model::Transaction transaction) {
+      writer.StartObject();
+
+      writer.String("signatures");
+      writer.StartArray();
+      for (auto sig : transaction.signatures) {
+        serialize(writer, sig);
+      }
+      writer.EndArray();
+
+      writer.String("created_ts");
+      writer.Uint64(transaction.created_ts);
+
+      writer.String("creator_account_id");
+      writer.String(transaction.creator_account_id.c_str());
+
+      writer.String("tx_counter");
+      writer.Uint64(transaction.tx_counter);
+
+      writer.String("commands");
+      writer.StartArray();
+      for (auto command : transaction.commands) {
+        serialize(writer, *command);
+      }
+      writer.EndArray();
+
+      writer.EndObject();
+    }
+
+    void BlockSerializer::serialize(PrettyWriter<StringBuffer>& writer,
+                                    model::Command& command) {
+      if (instanceof <model::AddPeer>(&command)) {
+        auto add_peer = static_cast<model::AddPeer&>(command);
+        writer.StartObject();
+
+        writer.String("command_type");
+        writer.String("AddPeer");
+
+        writer.String("address");
+        writer.String(add_peer.address.c_str());
+
+        writer.String("peer_key");
+        writer.String(add_peer.peer_key.to_string().c_str());
+
+        writer.EndObject();
+      }
+      if (instanceof <model::AddAssetQuantity>(&command)) {
+        auto add_asset_quantity =
+            static_cast<model::AddAssetQuantity&>(command);
+        writer.StartObject();
+
+        writer.String("command_type");
+        writer.String("AddAssetQuantity");
+
+        writer.String("account_id");
+        writer.String(add_asset_quantity.account_id.c_str());
+
+        writer.String("asset_id");
+        writer.String(add_asset_quantity.asset_id.c_str());
+
+        writer.String("amount");
+
+        writer.Double(
+            std::decimal::decimal64_to_double(add_asset_quantity.amount));
+
+        writer.EndObject();
+      }
+    }
+  }
+}
---
+++ test/module/irohad/ametsuchi/block_serializer_test.cpp
@@ -0,0 +1,100 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <ametsuchi/block_serializer.hpp>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/prettywriter.h>
+#include <model/commands/add_peer.hpp>
+#include <model/commands/add_asset_quantity.hpp>
+
+iroha::model::Signature create_signature();
+iroha::model::Transaction create_transaction();
+iroha::model::Proposal create_proposal();
+iroha::model::Block create_block();
+
+iroha::model::Signature create_signature() {
+  iroha::model::Signature signature{};
+  memset(signature.signature.data(), 0x123, iroha::ed25519::sig_t::size());
+  memset(signature.pubkey.data(), 0x123, iroha::ed25519::pubkey_t::size());
+  return signature;
+}
+
+iroha::model::Transaction create_transaction() {
+  iroha::model::Transaction tx{};
+  tx.creator_account_id = "123";
+
+  tx.tx_counter = 0;
+  tx.created_ts = 0;
+
+  tx.signatures.push_back(create_signature());
+  tx.signatures.push_back(create_signature());
+
+  //  tx.commands
+
+  // Add peer
+  iroha::model::AddPeer add_peer;
+  add_peer.address = "localhost";
+  std::fill(add_peer.peer_key.begin(), add_peer.peer_key.end(), 0x123);
+  tx.commands.push_back(std::make_shared<iroha::model::AddPeer>(add_peer));
+
+  iroha::model::AddAssetQuantity add_asset_qty;
+  add_asset_qty.account_id = "123";
+  add_asset_qty.asset_id = "123";
+  add_asset_qty.amount = std::decimal::make_decimal64(1010LL, -2);
+  tx.commands.push_back(std::make_shared<iroha::model::AddAssetQuantity>(add_asset_qty));
+
+  return tx;
+}
+
+iroha::model::Proposal create_proposal() {
+  std::vector<iroha::model::Transaction> txs;
+  txs.push_back(create_transaction());
+  txs.push_back(create_transaction());
+
+  iroha::model::Proposal proposal(txs);
+  return proposal;
+}
+
+iroha::model::Block create_block() {
+  iroha::model::Block block{};
+  memset(block.hash.data(), 0x123, iroha::ed25519::pubkey_t::size());
+  block.sigs.push_back(create_signature());
+  block.created_ts = 0;
+  block.height = 0;
+  memset(block.prev_hash.data(), 0x123, iroha::ed25519::pubkey_t::size());
+  block.txs_number = 0;
+  memset(block.merkle_root.data(), 0x123, iroha::ed25519::pubkey_t::size());
+  block.transactions.push_back(create_transaction());
+  return block;
+}
+
+
+TEST(block_serialize, block_serialize_test){
+
+  auto block = create_block();
+
+  iroha::ametsuchi::BlockSerializer blockSerializer;
+
+  rapidjson::StringBuffer sb;
+  rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(sb);
+  blockSerializer.serialize(writer, block);
+
+  std::cout << sb.GetString();
+}
