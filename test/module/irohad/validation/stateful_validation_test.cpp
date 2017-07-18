90c86062a26aa1f2a93aaefa6c7c9a7793d7da42
Bulat Nasrulin <bulat.nasrulin@gmail.com>
2017-07-18 16:25:25 +0300

    Add tests for set_quorum

--- test/module/irohad/validation/stateful_validation_test.cpp
+++ test/module/irohad/validation/stateful_validation_test.cpp
@@ -26,6 +26,7 @@
 #include "model/commands/create_asset.hpp"
 #include "model/commands/remove_signatory.hpp"
 #include "model/commands/set_permissions.hpp"
+#include "model/commands/set_quorum.hpp"
 
 auto ADMIN_ID = "admin@test";
 auto ACCOUNT_ID = "test@test";
@@ -478,4 +479,41 @@ TEST(CommandValidation, set_account_permissions) {
   command.account_id = "noacc";
   ASSERT_FALSE(command.validate(test_wsv, creator) &&
                command.execute(test_wsv, test_commands));
+}
+
+TEST(CommandValidation, set_quorum) {
+  WSVQueriesMock test_wsv;
+  WSVCommandsMock test_commands;
+
+  auto creator = get_default_creator();
+  auto orig_account = get_default_account();
+
+  iroha::model::SetQuorum command;
+
+  set_default_wsv(test_wsv, test_commands);
+  // Valid cases:
+  // 1. Creator is admin
+  creator.permissions.set_quorum = true;
+  command.account_id = ACCOUNT_ID;
+  command.new_quorum = 2;
+
+  ASSERT_TRUE(command.validate(test_wsv, creator) &&
+      command.execute(test_wsv, test_commands));
+
+  // Creator is the account
+  creator = orig_account;
+
+  ASSERT_TRUE(command.validate(test_wsv, creator) &&
+      command.execute(test_wsv, test_commands));
+  //--------- Non valid cases:--------------
+  // 1. Creator has no permissions
+  creator = get_default_creator();
+  ASSERT_FALSE(command.validate(test_wsv, creator) &&
+      command.execute(test_wsv, test_commands));
+
+  // 2.No such account exits
+  creator.permissions.set_quorum = true;
+  command.account_id = "noacc";
+  ASSERT_FALSE(command.validate(test_wsv, creator) &&
+      command.execute(test_wsv, test_commands));
 }
