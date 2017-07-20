aa7103108d0daa3c2858c3b8f152c474fbdd8c03
kamilsa <kamilsa16@gmail.com>
2017-07-20 20:52:30 +0300

    Implementation of ordering service

---
+++ irohad/ordering/impl/ordering_service_impl.cpp
@@ -0,0 +1,42 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ordering/impl/ordering_service_impl.hpp"
+
+namespace iroha {
+  namespace ordering {
+    void OrderingServiceImpl::propagate_transaction(
+        const model::Transaction &transaction) {
+      queue.push(transaction);
+    }
+
+    rxcpp::observable<model::Proposal> OrderingServiceImpl::on_proposal() {
+      return proposals_.get_observable();
+    }
+
+
+    void OrderingServiceImpl::generateProposal() {
+      std::vector<model::Transaction> txs;
+
+      for (model::Transaction tx; queue_.try_pop(tx) && txs.size() <= max_size_; ){
+        txs.push_back(tx);
+      }
+
+      proposals_.get_subscriber().on_next(model::Proposal(txs));
+    }
+  }
+}
---
+++ irohad/ordering/impl/ordering_service_impl.hpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_ORDERING_SERVICE_IMPL_HPP
+#define IROHA_ORDERING_SERVICE_IMPL_HPP
+
+#include "ordering/ordering_service.hpp"
+#include <tbb/concurrent_queue.h>
+#include "model/transaction.hpp"
+
+namespace iroha {
+  namespace ordering {
+
+  class OrderingServiceImpl : public OrderingService{
+   public:
+    void propagate_transaction(const model::Transaction &transaction) override;
+    rxcpp::observable<model::Proposal> on_proposal() override;
+
+    void generateProposal();
+   private:
+    tbb::concurrent_queue<model::Transaction> queue_;
+    const uint16_t max_size_; // max number of txs in proposal
+    rxcpp::subjects::subject<model::Proposal> proposals_;
+  };
+
+  }
+}
+
+#endif  // IROHA_ORDERING_SERVICE_IMPL_HPP
