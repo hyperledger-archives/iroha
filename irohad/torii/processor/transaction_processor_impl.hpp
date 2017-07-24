f8be4556514323478f7afc78a83594f559add71d
kamilsa <kamilsa16@gmail.com>
2017-07-24 13:59:42 +0300

    Change impl to interface in header

--- irohad/torii/processor/transaction_processor_impl.hpp
+++ irohad/torii/processor/transaction_processor_impl.hpp
@@ -22,7 +22,7 @@
 #include <model/model_crypto_provider.hpp>
 #include <model/transaction_response.hpp>
 #include <torii/processor/transaction_processor.hpp>
-#include <validation/impl/stateless_validator_impl.hpp>
+#include <validation/stateless_validator.hpp>
 #include <ordering/ordering_service.hpp>
 
 namespace iroha {
