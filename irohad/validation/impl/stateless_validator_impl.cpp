7394ebf48523322f5b3afa82507cd0371e38d973
kamilsa <kamilsa16@gmail.com>
2017-07-24 11:39:12 +0300

    Verify query in stateless validator

--- irohad/validation/impl/stateless_validator_impl.cpp
+++ irohad/validation/impl/stateless_validator_impl.cpp
@@ -51,6 +51,27 @@ namespace iroha {
     }
 
     bool StatelessValidatorImpl::validate(const model::Query& query) const {
+      // signatures are correct
+      {
+        if (!crypto_provider_.verify(query)) return false;
+      }
+
+      // time between creation and validation of the query
+      std::chrono::milliseconds now =
+          std::chrono::duration_cast<std::chrono::milliseconds>(
+              std::chrono::system_clock::now().time_since_epoch());
+      {
+        if (now.count() - query.created_ts > MAX_DELAY) {
+          return false;
+        }
+      }
+
+      // query is not sent from future
+      {
+        if (now.count() < query.created_ts) {
+          return false;
+        }
+      }
       return true;
     }
   }
