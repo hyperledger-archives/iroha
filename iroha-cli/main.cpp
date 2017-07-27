75a6f07b667efaf8d8fb54e9f37e24a6e70b539d
motxx <motohiko.ave@gmail.com>
2017-07-28 06:06:44 +0900

    Fix GenesisBlockService namespace

--- iroha-cli/main.cpp
+++ iroha-cli/main.cpp
@@ -20,22 +20,20 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
-#include "torii/command_client.hpp"
 #include <rapidjson/document.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/istreamwrapper.h>
-#include <logger/logger.hpp>
+#include <rapidjson/writer.h>
+#include "logger/logger.hpp"
+#include "ametsuchi/impl/storage_impl.hpp"
+#include "genesis_block_client.hpp"
+#include "genesis_block_service.hpp"
+#include "validators.hpp"
+#include "model/transaction.hpp"
+#include "model/block.hpp"
+#include "ametsuchi/block_serializer.hpp"
 
-DEFINE_bool(new_account, false, "Choose if account does not exist");
-DEFINE_string(name, "", "Name of the account");
-
-
-DEFINE_bool(grpc, false, "send sample transaction to IrohaNetwork");
-DEFINE_string(address, "127.0.0.1", "Where is Iroha network");
-DEFINE_int32(port, 50051, "What port to access iroha's Torii");
-
-DEFINE_string(config, "", "Information of Iroha’s peers for network bootstrapping")
-DEFINE_string(genesis_block, "", "Genesis block for sending network")
+// ** Genesis Block and Provisioning ** //
 
 void create_account(std::string name);
 
@@ -44,53 +42,113 @@ void create_account(std::string name);
 
 logger::Logger Log("iroha-cli");
 
+DEFINE_string(config, "", "Trusted peers' ip");
+DEFINE_validator(config, &iroha_cli::validate_config);
+
+DEFINE_string(genesis_block, "", "Genesis block for sending network");
+DEFINE_validator(genesis_block, &iroha_cli::validate_genesis_block);
+
+DEFINE_bool(new_account, false, "Choose if account does not exist");
+DEFINE_string(name, "", "Name of the account");
+
+void fatal_error(std::string const& error) {
+  Log.error(error);
+  exit(-1);
+}
+
+void assert_fatal(bool condition, std::string const& error) {
+  if (!condition) {
+    fatal_error(error);
+  }
+}
+
+std::vector<std::string> parse_config_trusted_peers(std::ifstream& ifs) {
+  std::vector<std::string> ret;
+  rapidjson::Document doc;
+  rapidjson::IStreamWrapper isw(ifs);
+  doc.ParseStream(isw);
+  assert_fatal(doc.HasParseError(), "JSON Parse error: " + FLAGS_config);
+
+  const char* MemberIp = "ip";
+  assert_fatal(doc.HasMember(MemberIp), "In '" + FLAGS_config +
+    "', member '" + std::string(MemberIp) + "' doesn't exist.");
+  for (const auto& ip : doc["ip"].GetArray()) {
+    assert_fatal(ip.IsString(), "'" + std::string(MemberIp) + "' has not string value.");
+    ret.push_back(ip.GetString());
+  }
+  return ret;
+}
+
+iroha::model::Block parse_genesis_block(std::ifstream &ifs) {
+  const char* MemberTxs = "transactions";
+
+  rapidjson::Document doc;
+  rapidjson::IStreamWrapper isw(ifs);
+  doc.ParseStream(isw);
+
+  assert_fatal(doc.HasParseError(), FLAGS_config + ": parse error");
+  assert_fatal(doc.IsObject(),      "JSON is not object.");
+  assert_fatal(doc.HasMember(MemberTxs), "No member '" + std::string(MemberTxs) + "'");
+  assert_fatal(doc[MemberTxs].IsArray(), std::string(MemberTxs) + " is not array.");
+
+  iroha::model::Block block;
+  auto block_serializer = iroha::ametsuchi::BlockSerializer();
+  auto txs = block_serializer.deserialize_transactions(doc);
+  assert_fatal(txs.has_value(), "Failed to deserialize transaction");
+  block.transactions = *txs;
+  // TODO: Add more members to block.
+  return block;
+}
+
+void abort_network(std::vector<std::string> const& trusted_peers, iroha::model::Block &block) {
+  for (const auto& ip : trusted_peers) {
+    iroha_cli::GenesisBlockClient client(ip, iroha::GenesisBlockServicePort);
+    client.SendAbortGenesisBlock(block);
+  }
+}
+
+void bootstrap_network() {
+  /**
+   * parse transactions from `genesis.json` (FLAGS_genesis_block)
+   * and then send block to trusted peers `target.json` (FLAGS_config)
+   * where each iroahd already wakes up.
+   */
+  std::ifstream ifs_config(FLAGS_config);
+  assert_fatal(ifs_config.is_open(), "Cannot open: '" + FLAGS_config + "'");
+  auto trusted_peers = parse_config_trusted_peers(ifs_config);
+
+  std::ifstream ifs_genesis(FLAGS_genesis_block);
+  assert_fatal(ifs_genesis.is_open(), "Cannot open: '" + FLAGS_genesis_block + "'");
+  auto genesis_block = parse_genesis_block(ifs_genesis);
+
+  // send block to trusted peers.
+  for (const auto& ip : trusted_peers) {
+    iroha_cli::GenesisBlockClient client(ip, iroha::GenesisBlockServicePort);
+    iroha::protocol::ApplyGenesisBlockResponse response;
+    auto stat = client.SendGenesisBlock(genesis_block, response);
+    if (!stat.ok() || response.applied() == iroha::protocol::APPLY_FAILURE) {
+      abort_network(trusted_peers, genesis_block);
+      assert_fatal(false, "Failure of creating genesis block in Ip: '" + ip + "'");
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
-
   gflags::ShutDownCommandLineFlags();
 
   if (FLAGS_new_account) {
     // Create new pub/priv key
     if (std::ifstream(FLAGS_name + ".pub")) {
-      std::cout << "File already exists" << std::endl;
-      return -1;
+      assert_fatal(false, "File already exists");
     }
     create_account(FLAGS_name);
   }
-  else if (!FLAGS_config.empty()) {
-    if (FLAGS_genesis_block.empty()) {
-      // iroha.conf
-      rapidjson::Document config;
-      std::ifstream ifs(FLAGS_config);
-      if (ifs.is_open()){
-        rapidjson::IStreamWrapper isw(ifs);
-        config.ParseStream(isw);
-        if (config.HasParseError()){
-          Log.error(FLAGS_config + ": parse error");
-          exit(1);
-        }
-      } else {
-        Log.error("");
-      }
-
-      /*
-        {
-        　 "peers":[
-            ["publickeyhere","192.168.0.3"],
-            ["publickeyhere","192.168.0.4",]
-            ["publickeyhere","192.168.0.5",]
-            ["publickeyhere","192.168.0.6"]
-          ],x
-          "ametsuchi_path":"/var/iroha/db",
-          "kaypair_path"  :"/var/iroha/",
-          ...
-        }
-       */
-
-    }
-    else {
-
-    }
+  else if (!FLAGS_config.empty() && !FLAGS_genesis_block.empty()) {
+    bootstrap_network();
+  }
+  else {
+    assert_fatal(false, "Invalid flags");
   }
 
   /*
@@ -108,6 +166,27 @@ int main(int argc, char* argv[]) {
     }
   }
   */
+
+  /*
+  if (FLAGS_new_ledger) {
+    auto storage = iroha::ametsuchi::StorageImpl::create(
+        FLAGS_path, FLAGS_redis, FLAGS_redis_port, FLAGS_pg_conn);
+    auto mut = storage->createMutableStorage();
+     auto block;
+     storage.apply(block, [](const auto& current_block, auto& executor,
+                             auto& query, auto& top_block) {
+       for (const auto& tx : current_block.transactions) {
+         for (const auto& command : tx.commands) {
+           if (not command->execute(query, executor)) {
+             return false;
+           }
+         }
+       }
+       return true;
+     });
+  }
+*/
+  return 0;
 }
 
 std::string hex_str(unsigned char* data, int len) {
