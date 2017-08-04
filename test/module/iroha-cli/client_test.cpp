a2048258885d42c59b6a2d47c3af2ca107a00f83
grimadas <bulat.nasrulin@gmail.com>
2017-08-04 19:11:30 +0300

    Add test cases for queries in client test

--- test/module/iroha-cli/client_test.cpp
+++ test/module/iroha-cli/client_test.cpp
@@ -15,6 +15,7 @@
  * limitations under the License.
  */
 
+#include <responses.pb.h>
 #include "module/irohad/ametsuchi/ametsuchi_mocks.hpp"
 #include "module/irohad/network/network_mocks.hpp"
 #include "module/irohad/validation/validation_mocks.hpp"
@@ -164,4 +165,126 @@ TEST_F(ClientTest, SendTxWhenStatelessInvalid) {
       "                }]}";
   std::cout << "Sending  json transaction to Iroha" << std::endl;
   ASSERT_EQ(client.sendTx(json_tx), iroha_cli::CliClient::NOT_VALID);
+}
+
+TEST_F(ClientTest, SendQueryWhenInvalidJson) {
+  iroha_cli::CliClient client(Ip, Port);
+  // Must not call stateful validation
+  EXPECT_CALL(svMock, validate(A<std::shared_ptr<const iroha::model::Query>>()))
+      .Times(0);
+  auto json_query =
+      "{\n"
+      "  \"creator_account_id\": \"test\", \n"
+      "  \"commands\":[{\n"
+      "  \"command_type\": \"AddPeer\",\n"
+      "    \"address\": \"localhost\",\n"
+      "    \"peer_key\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323\"\n"
+      "  }]\n"
+      "}";
+  std::cout << "Sending  json query to Iroha" << std::endl;
+  auto res = client.sendQuery(json_query);
+  ASSERT_TRUE(res.has_error_response());
+  ASSERT_EQ(res.error_response().reason(),
+            iroha::protocol::ErrorResponse::WRONG_FORMAT);
+}
+
+TEST_F(ClientTest, SendQueryWhenStatelessInvalid) {
+  iroha_cli::CliClient client(Ip, Port);
+  // Must not call stateful validation
+  EXPECT_CALL(svMock, validate(A<std::shared_ptr<const iroha::model::Query>>()))
+      .WillOnce(Return(false));
+  auto json_query =
+      "{\"signature\": {\n"
+      "                    \"pubkey\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323\",\n"
+      "                    \"signature\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323232323"
+      "2323232323232323232323232323232323232323232323232323232323\"\n"
+      "                }, \n"
+      "            \"created_ts\": 0,\n"
+      "            \"creator_account_id\": \"123\",\n"
+      "            \"query_counter\": 0,\n"
+      "            \"query_type\": \"GetAccount\",\n"
+      "            \"account_id\": \"test@test\"\n"
+      "                }";
+  std::cout << "Sending  json query to Iroha" << std::endl;
+  auto res = client.sendQuery(json_query);
+  ASSERT_TRUE(res.has_error_response());
+  ASSERT_EQ(res.error_response().reason(),
+            iroha::model::ErrorResponse::STATELESS_INVALID);
+}
+
+TEST_F(ClientTest, SendQueryWhenValid) {
+  iroha_cli::CliClient client(Ip, Port);
+  // Must not call stateful validation
+  EXPECT_CALL(svMock, validate(A<std::shared_ptr<const iroha::model::Query>>()))
+      .WillOnce(Return(true));
+  auto account_admin = iroha::model::Account();
+  account_admin.account_id = "admin@test";
+  account_admin.permissions.read_all_accounts = true;
+
+  auto account_test = iroha::model::Account();
+  account_test.account_id = "test@test";
+
+  EXPECT_CALL(wsv_query, getAccount("admin@test"))
+      .WillOnce(Return(account_admin));
+  EXPECT_CALL(wsv_query, getAccount("test@test"))
+      .WillOnce(Return(account_test));
+
+  auto json_query =
+      "{\"signature\": {\n"
+      "                    \"pubkey\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323\",\n"
+      "                    \"signature\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323232323"
+      "2323232323232323232323232323232323232323232323232323232323\"\n"
+      "                }, \n"
+      "            \"created_ts\": 0,\n"
+      "            \"creator_account_id\": \"admin@test\",\n"
+      "            \"query_counter\": 0,\n"
+      "            \"query_type\": \"GetAccount\",\n"
+      "            \"account_id\": \"test@test\"\n"
+      "                }";
+  std::cout << "Sending  json query to Iroha" << std::endl;
+  auto res = client.sendQuery(json_query);
+  ASSERT_TRUE(res.has_account_response());
+  ASSERT_EQ(res.account_response().account().account_id(), "test@test");
+}
+
+TEST_F(ClientTest, SendQueryWhenStatefulInvalid) {
+  iroha_cli::CliClient client(Ip, Port);
+  // Must not call stateful validation
+  EXPECT_CALL(svMock, validate(A<std::shared_ptr<const iroha::model::Query>>()))
+      .WillOnce(Return(true));
+  auto account_admin = iroha::model::Account();
+  account_admin.account_id = "admin@test";
+
+  auto account_test = iroha::model::Account();
+  account_test.account_id = "test@test";
+
+  EXPECT_CALL(wsv_query, getAccount("admin@test"))
+      .WillOnce(Return(account_admin));
+  EXPECT_CALL(wsv_query, getAccount("test@test"))
+      .Times(0);
+
+  auto json_query =
+      "{\"signature\": {\n"
+      "                    \"pubkey\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323\",\n"
+      "                    \"signature\": "
+      "\"2323232323232323232323232323232323232323232323232323232323232323232323"
+      "2323232323232323232323232323232323232323232323232323232323\"\n"
+      "                }, \n"
+      "            \"created_ts\": 0,\n"
+      "            \"creator_account_id\": \"admin@test\",\n"
+      "            \"query_counter\": 0,\n"
+      "            \"query_type\": \"GetAccount\",\n"
+      "            \"account_id\": \"test@test\"\n"
+      "                }";
+  std::cout << "Sending  json query to Iroha" << std::endl;
+  auto res = client.sendQuery(json_query);
+  ASSERT_TRUE(res.has_error_response());
+  ASSERT_EQ(res.error_response().reason(),
+            iroha::protocol::ErrorResponse::STATEFUL_INVALID);
 }
