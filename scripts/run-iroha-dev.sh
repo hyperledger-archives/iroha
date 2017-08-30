0d2dee5ae8ca8aec8a099a0424d2dc226e368ae9
Andrei Lebedev <lebdron@gmail.com>
2017-08-30 23:39:06 +0800

    Add support for dynamic docker project name

--- scripts/run-iroha-dev.sh
+++ scripts/run-iroha-dev.sh
@@ -7,17 +7,19 @@ CURDIR="$(cd "$(dirname "$0")"; pwd)"
 IROHA_HOME="$(dirname "${CURDIR}")"
 PROJECT=iroha${USERID}
 
+export COMPOSE_PROJECT_NAME=${PROJECT}
+
 if [ ! "$(docker ps -q -f name=${PROJECT}_node_1)" ] # node already running
 then
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} up -d
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} exec \
-    node adduser --disabled-password --gecos "" ${USER} --uid ${USERID}
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} exec \
-    node chown -R ${USER}:${USER} /tmp/ccache
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} exec \
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml up -d
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml exec node \
+    adduser --disabled-password --gecos "" ${USER} --uid ${USERID}
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml exec node \
+    chown -R ${USER}:${USER} /tmp/ccache
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml exec \
     --user ${USER} node /bin/bash
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} down
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml down
 else
-  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml -p ${PROJECT} exec \
+  docker-compose -f ${IROHA_HOME}/docker/docker-compose.yml exec \
     --user ${USER} node /bin/bash
 fi
