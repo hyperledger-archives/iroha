0a38c3b0bfa3b9a0a5477b8690f4c8b00de00ac4
Fyodor Muratov <muratovfyodor@yandex.ru>
2017-09-22 12:28:41 +0300

    Fix missed bracket in mock

--- irohad/main/application.cpp
+++ irohad/main/application.cpp
@@ -31,7 +31,7 @@ using namespace iroha::consensus::yac;
 
 class MockCryptoProvider : public ModelCryptoProvider {
  public:
-  MOCK_CONST_METHOD1(verify, bool(const Transaction &)
+  MOCK_CONST_METHOD1(verify, bool(const Transaction &));
   MOCK_CONST_METHOD1(verify, bool(std::shared_ptr<const Query>));
   MOCK_CONST_METHOD1(verify, bool(const Block &));
 };
