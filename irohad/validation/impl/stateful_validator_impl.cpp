3b178e7ca8e390d737867b53f7bfb8b6d44d360b
kamilsa <kamilsa16@gmail.com>
2017-09-30 13:49:48 +0300

    Review changes

--- irohad/validation/impl/stateful_validator_impl.cpp
+++ irohad/validation/impl/stateful_validator_impl.cpp
@@ -32,13 +32,13 @@ namespace iroha {
       auto checking_transaction = [](auto &tx, auto &queries) {
         auto account = queries.getAccount(tx.creator_account_id);
         // Check if tx creator has account and has quorum to execute transaction
-        if (!account || tx.signatures.size() < account.value().quorum) {
+        if (not account.has_value() || tx.signatures.size() < account.value().quorum) {
           return false;
         }
 
         // Check if signatures in transaction are account signatory
         auto account_signs = queries.getSignatories(tx.creator_account_id);
-        if (not account_signs) {
+        if (not account_signs.has_value()) {
           // No signatories found
           return false;
         }
