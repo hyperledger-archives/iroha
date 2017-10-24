f0fe1fe74ae5f2ef8576cbe877bf611259e5ca1d
Kitsu <mail@kitsu.me>
2017-10-24 15:14:03 +0300

    Review fixes
    Extract monadic transforms when possible
    Replace for-range loop with boost:irange
    Replace std::random_shuffle with std::shuffle
    
    Signed-off-by: Kitsu <mail@kitsu.me>

--- irohad/multi_sig_transactions/impl/gossip_propagation_strategy.cpp
+++ irohad/multi_sig_transactions/impl/gossip_propagation_strategy.cpp
@@ -17,7 +17,9 @@
 
 #include "multi_sig_transactions/gossip_propagation_strategy.hpp"
 #include <boost/assert.hpp>
+#include <boost/range/irange.hpp>
 #include <numeric>
+#include <random>
 #include <vector>
 
 namespace iroha {
@@ -33,11 +35,15 @@ namespace iroha {
         emitent(rxcpp::observable<>::interval(steady_clock::now(), period)
                     .map([this, amount](int) {
                       PropagationData vec;
-                      for (uint32_t i = 0; i < amount; i++) {
-                        OptPeer element = this->visit();
-                        if (not element) break;
-                        vec.push_back(*element);
-                      }
+                      auto range = boost::irange(0u, amount);
+                      // push until find empty element
+                      std::find_if_not(
+                          range.begin(), range.end(), [this, &vec](auto) {
+                            return this->visit() | [&vec](auto e) -> bool {
+                              vec.push_back(e);
+                              return true;  // proceed
+                            };
+                          });
                       return vec;
                     })) {}
 
@@ -46,23 +52,23 @@ namespace iroha {
   }
 
   bool GossipPropagationStrategy::initQueue() {
-    return static_cast<bool>(
-        query->getLedgerPeers() |
-            [this](PropagationData data) -> boost::optional<int> {
-          last_data = data;
-          if (data.size() == 0) return {};
-          this->non_visited.resize(this->last_data.size());
-          std::iota(this->non_visited.begin(), this->non_visited.end(), 0);
-          std::random_shuffle(this->non_visited.begin(),
-                              this->non_visited.end());
-          return {0}; // any value on optional derefernce gives true
-        });
+    return query->getLedgerPeers() |
+               [](auto &data) -> boost::optional<PropagationData> {
+      if (data.size() == 0) return {};
+      return data;
+    } | [this](auto &data) -> bool {  // nullopt implicitly casts to false
+      this->last_data = std::move(data);
+      this->non_visited.resize(this->last_data.size());
+      std::iota(this->non_visited.begin(), this->non_visited.end(), 0);
+      std::shuffle(this->non_visited.begin(),
+                   this->non_visited.end(),
+                   std::default_random_engine{});
+      return true;
+    };
   }
 
   OptPeer GossipPropagationStrategy::visit() {
-    if (non_visited.empty() and not initQueue()) {
-      return {};
-    }
+    if (non_visited.empty() and not initQueue()) return {};
     // either initQueue exits from method or non_visited non-empty
     BOOST_ASSERT(not non_visited.empty());
 
