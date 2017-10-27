562500cd610c1b22708844458442a9a7c43e4fdf
Kitsu <mail@kitsu.me>
2017-10-27 19:12:23 +0300

    Removed this capture in redis query
    Signed-off-by: Kitsu <mail@kitsu.me>

--- irohad/ametsuchi/impl/redis_block_query.cpp
+++ irohad/ametsuchi/impl/redis_block_query.cpp
@@ -72,17 +72,16 @@ namespace iroha {
     std::vector<iroha::model::Block::BlockHeightType>
     RedisBlockQuery::getBlockIds(const std::string &account_id) {
       std::vector<uint64_t> block_ids;
-      client_.lrange(
-          account_id, 0, -1, [this, &block_ids](cpp_redis::reply &reply) {
-            for (const auto &block_reply : reply.as_array()) {
-              const auto &string_reply = block_reply.as_string();
+      client_.lrange(account_id, 0, -1, [&block_ids](cpp_redis::reply &reply) {
+        for (const auto &block_reply : reply.as_array()) {
+          const auto &string_reply = block_reply.as_string();
 
-              // check if reply is an integer
-              if (isdigit(string_reply.c_str()[0])) {
-                block_ids.push_back(std::stoul(string_reply));
-              }
-            }
-          });
+          // check if reply is an integer
+          if (isdigit(string_reply.c_str()[0])) {
+            block_ids.push_back(std::stoul(string_reply));
+          }
+        }
+      });
       client_.sync_commit();
       return block_ids;
     }
