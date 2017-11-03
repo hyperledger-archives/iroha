fcf3f092315f7263b3382f4c092c9fbddf4aa54a
Andrei Lebedev <lebdron@gmail.com>
2017-11-03 09:57:09 +0800

    Add test for receiving message from unknown peer
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- test/module/irohad/consensus/yac/network_test.cpp
+++ test/module/irohad/consensus/yac/network_test.cpp
@@ -18,52 +18,97 @@
 #include "module/irohad/consensus/yac/yac_mocks.hpp"
 
 #include <grpc++/grpc++.h>
-#include "consensus/yac/transport/impl/network_impl.hpp"
 
-using namespace iroha::consensus::yac;
-using iroha::model::Peer;
+#include "consensus/yac/transport/yac_pb_converters.hpp"
+#include "consensus/yac/transport/impl/network_impl.hpp"
 
 using ::testing::_;
 using ::testing::InvokeWithoutArgs;
 
-TEST(NetworkTest, MessageHandledWhenMessageSent) {
-  auto notifications = std::make_shared<MockYacNetworkNotifications>();
+namespace iroha {
+  namespace consensus {
+    namespace yac {
+      class YacNetworkTest : public ::testing::Test {
+       public:
+        void SetUp() override {
+          notifications = std::make_shared<MockYacNetworkNotifications>();
 
-  auto peer = mk_peer("0.0.0.0:50051");
-  std::vector<Peer> peers = {peer};
-  std::shared_ptr<NetworkImpl> network =
-      std::make_shared<NetworkImpl>(peer.address, peers);
+          peer = mk_peer("0.0.0.0:50051");
+          std::vector<model::Peer> peers = {peer};
+          network = std::make_shared<NetworkImpl>(peer.address, peers);
 
-  VoteMessage message;
-  message.hash.proposal_hash = "proposal";
-  message.hash.block_hash = "block";
+          message.hash.proposal_hash = "proposal";
+          message.hash.block_hash = "block";
 
-  std::mutex mtx;
-  std::condition_variable cv;
-  ON_CALL(*notifications, on_vote(peer, message))
-      .WillByDefault(
-          InvokeWithoutArgs(&cv, &std::condition_variable::notify_one));
+          network->subscribe(notifications);
 
-  EXPECT_CALL(*notifications, on_vote(peer, message)).Times(1);
+          grpc::ServerBuilder builder;
+          int port = 0;
+          builder.AddListeningPort(peer.address,
+                                   grpc::InsecureServerCredentials(),
+                                   &port);
+          builder.RegisterService(network.get());
+          server = builder.BuildAndStart();
+          ASSERT_TRUE(server);
+          ASSERT_NE(port, 0);
+        }
 
-  network->subscribe(notifications);
+        void TearDown() override {
+          server->Shutdown();
+        }
 
-  std::unique_ptr<grpc::Server> server;
+        std::shared_ptr<MockYacNetworkNotifications> notifications;
+        std::shared_ptr<NetworkImpl> network;
+        model::Peer peer;
+        VoteMessage message;
+        std::unique_ptr<grpc::Server> server;
+        std::mutex mtx;
+        std::condition_variable cv;
+      };
 
-  grpc::ServerBuilder builder;
-  int port = 0;
-  builder.AddListeningPort(peer.address, grpc::InsecureServerCredentials(),
-                           &port);
-  builder.RegisterService(network.get());
-  server = builder.BuildAndStart();
-  ASSERT_TRUE(server);
-  ASSERT_NE(port, 0);
+      /**
+       * @given initialized network
+       * @when send vote to itself
+       * @then vote handled
+       */
+      TEST_F(YacNetworkTest, MessageHandledWhenMessageSent) {
+        EXPECT_CALL(*notifications, on_vote(peer, message))
+            .Times(1)
+            .WillRepeatedly(
+                InvokeWithoutArgs(&cv, &std::condition_variable::notify_one));
 
-  network->send_vote(peer, message);
+        network->send_vote(peer, message);
 
-  // wait for response reader thread
-  std::unique_lock<std::mutex> lock(mtx);
-  cv.wait_for(lock, std::chrono::milliseconds(100));
+        // wait for response reader thread
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait_for(lock, std::chrono::milliseconds(100));
+      }
 
-  server->Shutdown();
-}
+      /**
+       * @given initialized network
+       * @when send vote from previously unknown peer
+       * @then vote handled
+       */
+      TEST_F(YacNetworkTest, MesssageHandledWhenUnknownPeer) {
+        auto client = proto::Yac::NewStub(grpc::CreateChannel(
+            peer.address, grpc::InsecureChannelCredentials()));
+
+        EXPECT_CALL(*notifications, on_vote(_, _))
+            .Times(1)
+            .WillRepeatedly(
+                InvokeWithoutArgs(&cv, &std::condition_variable::notify_one));
+
+        grpc::ClientContext context;
+        context.AddMetadata("address", "0.0.0.0:50052");
+        auto vote = PbConverters::serializeVote(message);
+        google::protobuf::Empty response;
+
+        client->SendVote(&context, vote, &response);
+
+        // wait for response reader thread
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait_for(lock, std::chrono::milliseconds(100));
+      }
+    } // namespace yac
+  } // namespace consensus
+} // namespace iroha
