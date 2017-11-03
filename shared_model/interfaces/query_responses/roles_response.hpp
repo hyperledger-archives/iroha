a65d7870d9b70260f7f72ea62b4cf7cb4a14b34a
motxx <motohiko.ave@gmail.com>
2017-11-04 04:37:35 +0900

    Add RolesResponse
    Signed-off-by: motxx <motohiko.ave@gmail.com>

---
+++ shared_model/interfaces/query_responses/roles_response.hpp
@@ -0,0 +1,70 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_SHARED_MODEL_ROLES_RESPONSE_HPP
+#define IROHA_SHARED_MODEL_ROLES_RESPONSE_HPP
+
+#include "interfaces/common_objects/types.hpp"
+#include "interfaces/primitive.hpp"
+#include "model/queries/responses/roles_response.hpp"
+#include "utils/string_builder.hpp"
+
+namespace shared_model {
+  namespace interface {
+    /**
+     * Provide response with all roles of the current system
+     */
+    class RolesResponse
+        : public Primitive<RolesResponse, iroha::model::RolesResponse> {
+     public:
+      /**
+       * @return all roles of the current system
+       */
+      virtual const types::RolesType &roles() const = 0;
+
+      /**
+       * Stringify the data.
+       * @return string representation of data.
+       */
+      std::string toString() const override {
+        return detail::PrettyStringBuilder()
+            .init("RolesResponse")
+            .append("roles")
+            .appendAll(roles(), [](auto s) { return s; })
+            .finalize();
+      }
+
+      /**
+       * @return true if the data are same.
+       */
+      bool operator==(const ModelType &rhs) const override {
+        return roles() == rhs.roles();
+      }
+
+      /**
+       * Makes old model.
+       * @return An allocated old model of roles response.
+       */
+      OldModelType *makeOldModel() const override {
+        OldModelType *oldModel = new OldModelType();
+        oldModel->roles = roles();
+        return oldModel;
+      }
+    };
+  }  // namespace interface
+}  // namespace shared_model
+#endif  // IROHA_SHARED_MODEL_ROLES_RESPONSE_HPP
