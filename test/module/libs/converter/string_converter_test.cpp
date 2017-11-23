25ef507cf772029f763739e7474d535133010298
luckychess <toobwn@gmail.com>
2017-11-23 20:03:37 +0300

    Add 2 one-side tests for string converter.
    Signed-off-by: luckychess <toobwn@gmail.com>

--- test/module/libs/converter/string_converter_test.cpp
+++ test/module/libs/converter/string_converter_test.cpp
@@ -20,6 +20,19 @@
 
 using namespace iroha;
 
+TEST(StringConverterTest, ConvertHexToBinary) {
+  std::string hex = "ff000233551117daa110050399";
+  std::string bin = "\xFF\x02\x33\x55\x11\x17\xDA\xA1\x10\x05\x03\x99";
+  bin.insert(bin.begin() + 1, '\0');
+  ASSERT_EQ(hexstringToBytestring(hex).value(), bin);
+}
+
+TEST(StringConverterTest, ConvertBinaryToHex) {
+  std::string bin = "\xFF\x02\x33\x55\x11\x17\xDA\xA1\x10\x05\x03\x99";
+  bin.insert(bin.begin() + 1, '\0');
+  ASSERT_EQ(bytestringToHexstring(bin), "ff000233551117daa110050399");
+}
+
 TEST(StringConverterTest, ConvertHexToBinaryAndBack) {
   std::string hexString = "0123456789abcdef";
   ASSERT_EQ(hexString,
