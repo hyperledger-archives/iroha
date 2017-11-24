e689e8ecf0615a7f94be2244d8600c7e8e34c02a
luckychess <toobwn@gmail.com>
2017-11-24 11:24:03 +0300

    Add given/when/then comments to tests.
    Signed-off-by: luckychess <toobwn@gmail.com>

--- test/module/libs/converter/string_converter_test.cpp
+++ test/module/libs/converter/string_converter_test.cpp
@@ -21,17 +21,32 @@
 using namespace iroha;
 using namespace std::string_literals;
 
+/**
+ * @given hex string
+ * @when hex string was converted to binary string
+ * @then converted string match the result we are expected
+ */
 TEST(StringConverterTest, ConvertHexToBinary) {
   std::string hex = "ff000233551117daa110050399";
   std::string bin = "\xFF\x00\x02\x33\x55\x11\x17\xDA\xA1\x10\x05\x03\x99"s;
   ASSERT_EQ(hexstringToBytestring(hex).value(), bin);
 }
 
+/**
+ * @given binary string
+ * @when binary string was converted to hex string
+ * @then converted string match the result we are expected
+ */
 TEST(StringConverterTest, ConvertBinaryToHex) {
   std::string bin = "\xFF\x00\x02\x33\x55\x11\x17\xDA\xA1\x10\x05\x03\x99"s;
   ASSERT_EQ(bytestringToHexstring(bin), "ff000233551117daa110050399");
 }
 
+/**
+ * @given hex string of length 256 with all possible byte values
+ * @when convert it to byte string and back
+ * @then resulted string is the same as given one
+ */
 TEST(StringConverterTest, ConvertHexToBinaryAndBack) {
   std::stringstream ss;
   ss << std::hex << std::setfill('0');
