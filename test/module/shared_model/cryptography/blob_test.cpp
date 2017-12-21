155c5a1b0030c0f6dee719a8414f9c098248cbc8
Kitsu <mail@kitsu.me>
2017-12-21 15:48:47 +0300

    Add fixture in blob_test
    Signed-off-by: Kitsu <mail@kitsu.me>

--- test/module/shared_model/cryptography/blob_test.cpp
+++ test/module/shared_model/cryptography/blob_test.cpp
@@ -17,21 +17,25 @@
 
 #include "cryptography/blob.hpp"
 #include <gtest/gtest.h>
+#include <memory>
 
 using namespace shared_model::crypto;
 using namespace std::literals::string_literals;
 
-auto data = "Hello \0World"s;
+class BlobMock : public ::testing::Test {
+ public:
+  void SetUp() override { blob = std::make_shared<Blob>(data); }
+  std::shared_ptr<Blob> blob;
+  std::string data = "Hello \0World"s;
+};
 
 /**
  * @given arbitrary string and known its hex representation
  * @when conversion of this string to hex is done
  * @then conversion is done right
  */
-TEST(BlobTest, HexConversionTest) {
-  Blob blob(data);
-  auto hex = blob.hex();
-  ASSERT_EQ("48656c6c6f2000576f726c64", hex);
+TEST_F(BlobMock, HexConversionTest) {
+  ASSERT_EQ("48656c6c6f2000576f726c64", blob->hex());
 }
 
 /**
@@ -39,10 +43,9 @@ TEST(BlobTest, HexConversionTest) {
  * @when making a blob from it
  * @then make sure that the blob's blob stores the same data as string
  */
-TEST(BlobTest, BlobIsString) {
-  Blob blob(data);
-  auto bin_str = toBinaryString(blob);
-  auto binary = blob.blob();
+TEST_F(BlobMock, BlobIsString) {
+  auto bin_str = toBinaryString(*blob);
+  auto binary = blob->blob();
   size_t sz = binary.size();
 
   ASSERT_EQ(bin_str.size(), sz);
