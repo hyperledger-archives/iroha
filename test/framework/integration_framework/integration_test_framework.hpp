8510e8c30da1131b6cd21458930071037767214c
Fedor Muratov <muratovfyodor@yandex.ru>
2017-12-21 12:20:50 +0900

    * rework queues with tbb concurrent queues * add fetching for proposal and block
    Signed-off-by: Fedor Muratov <muratovfyodor@yandex.ru>

--- test/framework/integration_framework/integration_test_framework.hpp
+++ test/framework/integration_framework/integration_test_framework.hpp
@@ -18,7 +18,11 @@
 #ifndef IROHA_INTEGRATION_FRAMEWORK_HPP
 #define IROHA_INTEGRATION_FRAMEWORK_HPP
 
+#include <tbb/concurrent_queue.h>
+#include <chrono>
+#include <exception>
 #include <queue>
+#include <thread>
 #include "framework/integration_framework/iroha_instance.hpp"
 #include "logger/logger.hpp"
 #include "model/block.hpp"
@@ -26,7 +30,14 @@
 #include "model/transaction.hpp"
 
 namespace integration_framework {
+
+  using namespace std::chrono_literals;
+
   class IntegrationTestFramework {
+   private:
+    using ProposalType = std::shared_ptr<iroha::model::Proposal>;
+    using BlockType = std::shared_ptr<iroha::model::Block>;
+
    public:
     // init
     IntegrationTestFramework &setInitialState(
@@ -38,15 +49,29 @@ namespace integration_framework {
       iroha_instance_->clearLedger();
       log_->info("cleared ledger");
       iroha_instance_->rawInsertBlock(block);
-      log_->info("raw instert");
+      log_->info("raw insert");
+
       // subscribing for components
+
       iroha_instance_->getIrohaInstance()
           ->getPeerCommunicationService()
           ->on_proposal()
-          .subscribe([this](auto proposal) { proposal_queue_.push(proposal); });
+          .subscribe([this](auto proposal) {
+            proposal_queue_.push(
+                std::make_shared<iroha::model::Proposal>(proposal));
+          });
 
-      // iroha_instance_->getIrohaInstance()->getPeerCommunicationService()->on_commit().s
+      iroha_instance_->getIrohaInstance()
+          ->getPeerCommunicationService()
+          ->on_commit()
+          .subscribe([this](auto commit_observable) {
+            commit_observable.subscribe([this](auto committed_block) {
+              block_queue_.push(
+                  std::make_shared<iroha::model::Block>(committed_block));
+            });
+          });
 
+      // start instance
       iroha_instance_->run();
       log_->info("run iroha");
       return *this;
@@ -69,8 +94,8 @@ namespace integration_framework {
       google::protobuf::Empty response;
       iroha_instance_->getIrohaInstance()->getCommandService()->ToriiAsync(
           pb_tx, response);
-      // fetch first tx from queue with cv
-      // check status
+      // fetch status of transaction
+      // check validation function
       return *this;
     }
 
@@ -79,12 +104,14 @@ namespace integration_framework {
     IntegrationTestFramework &checkProposal(Lambda validation) {
       log_->info("check proposal");
       // fetch first proposal from proposal queue
-
+      ProposalType proposal;
+      fetchFromQueue(
+          proposal_queue_, proposal, proposal_waiting, "missed proposal");
       return *this;
     }
 
     IntegrationTestFramework &skipProposal() {
-      checkProposal([] {});
+      checkProposal([](const iroha::model::Proposal &) {});
       return *this;
     }
 
@@ -93,10 +120,13 @@ namespace integration_framework {
     IntegrationTestFramework &checkBlock(Lambda validation) {
       // fetch first from block queue
       log_->info("check block");
+      BlockType block;
+      fetchFromQueue(block_queue_, block, block_waiting, "missed block");
+      validation(*block);
       return *this;
     }
     IntegrationTestFramework &skipBlock() {
-      checkBlock([] {});
+      checkBlock([](const iroha::model::Block &) {});
       return *this;
     }
 
@@ -106,12 +136,43 @@ namespace integration_framework {
       iroha_instance_->clearLedger();
     }
 
+    /**
+     * general way to fetch object from concurrent queue
+     * @tparam Queue - Type of queue
+     * @tparam ObjectType - Type of fetched object
+     * @tparam WaitTime - time for waiting if data doesn't appear
+     * @param queue - queue instance for fetching
+     * @param ref_for_insertion - reference to insert object
+     * @param wait - time of waiting
+     * @param error_reason - reason if thehre is no appeared object at all
+     */
+    template <typename Queue, typename ObjectType, typename WaitTime>
+    static void fetchFromQueue(Queue &queue,
+                               ObjectType &ref_for_insertion,
+                               const WaitTime &wait,
+                               const std::string &error_reason) {
+      if (!queue.try_pop(ref_for_insertion)) {
+        std::this_thread::sleep_for(wait);
+      }
+      if (!queue.try_pop(ref_for_insertion)) {
+        throw std::runtime_error(error_reason);
+      }
+    }
+
    protected:
     std::shared_ptr<IrohaInstance> iroha_instance_ =
         std::make_shared<IrohaInstance>();
-    std::queue<int> status_queue_;
-    std::queue<iroha::model::Proposal> proposal_queue_;
-    std::queue<iroha::model::Block> block_queue_;
+    tbb::concurrent_queue<ProposalType> proposal_queue_;
+    tbb::concurrent_queue<BlockType> block_queue_;
+
+    // config area
+
+    /// maximum time of waiting before appearing next proposal
+    // TODO 21/12/2017 muratovv make relation of time with instance's config
+    const std::chrono::milliseconds proposal_waiting = 5000ms;
+
+    /// maximum time of waiting before appearing next committed block
+    const std::chrono::milliseconds block_waiting = 5000ms;
 
    private:
     logger::Logger log_ = logger::log("IntegrationTestFramework");
