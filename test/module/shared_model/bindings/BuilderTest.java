9ee4d80ea1ac653de60715eb0031db672f7408d4
Kitsu <mail@kitsu.me>
2017-12-23 14:32:27 +0300

    Add test protobuf deser in Java bindings
    Signed-off-by: Kitsu <mail@kitsu.me>

--- test/module/shared_model/bindings/BuilderTest.java
+++ test/module/shared_model/bindings/BuilderTest.java
@@ -3,8 +3,13 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 
+import iroha.protocol.Commands;
+import iroha.protocol.BlockOuterClass;
+import com.google.protobuf.InvalidProtocolBufferException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BuilderTest {
     static {
@@ -17,10 +22,10 @@ public class BuilderTest {
     }
 
     private Keypair keys;
-    private ModelBuilder builder;
+    private ModelTransactionBuilder builder;
 
-    ModelBuilder base() {
-        return new ModelBuilder().txCounter(BigInteger.valueOf(123))
+    ModelTransactionBuilder base() {
+        return new ModelTransactionBuilder().txCounter(BigInteger.valueOf(123))
                 .createdTime(BigInteger.valueOf(System.currentTimeMillis()))
                 .creatorAccountId("admin@test");
     }
@@ -37,7 +42,7 @@ public class BuilderTest {
 
     @Test
     void emptyTx() {
-        ModelBuilder builder = new ModelBuilder();
+        ModelTransactionBuilder builder = new ModelTransactionBuilder();
         assertThrows(IllegalArgumentException.class, builder::build);
     }
 
@@ -71,7 +76,7 @@ public class BuilderTest {
     void addPeerWithInvalidKeySize() {
         setAddPeer();
         for (String i : new String[]{"", "a", "1111111111111111111111111111111", "111111111111111111111111111111111"}) {
-            ModelBuilder builder = base().addPeer("123.123.123.123", new PublicKey(i));
+            ModelTransactionBuilder builder = base().addPeer("123.123.123.123", new PublicKey(i));
             assertThrows(IllegalArgumentException.class, builder::build);
         }
     }
@@ -80,13 +85,13 @@ public class BuilderTest {
     void addPeerWithInvalidHost() {
         setAddPeer();
         for (String i : new String[]{"257.257.257.257", "host#host", "asd@asd", "ab..cd"}) {
-            ModelBuilder builder = base().addPeer(i, keys.publicKey());
+            ModelTransactionBuilder builder = base().addPeer(i, keys.publicKey());
             assertThrows(IllegalArgumentException.class, builder::build);
         }
     }
 
-    long protoSize(UnsignedTx tx) {
-        return new ModelTransactionProto().signAndAddSignature(tx, keys).size();
+    Blob proto(UnsignedTx tx) {
+        return new ModelProtoTransaction().signAndAddSignature(tx, keys);
     }
 
     @Test
@@ -95,51 +100,74 @@ public class BuilderTest {
         assertEquals(keys.privateKey().size(), 64);
     }
 
+    /**
+     * Performs check that Blob contains valid proto Transaction
+     * @param serialized blob with binary data for check
+     * @return true if valid
+     */
+    private boolean checkProtoTx(Blob serialized) {
+        ByteVector blob = serialized.blob();
+        byte bs[] = new byte[(int)blob.size()];
+
+        for (int i = 0; i < blob.size(); i++) {
+            bs[i] = (byte)blob.get(i);
+        }
+
+        try {
+            BlockOuterClass.Transaction.parseFrom(bs);
+        } catch (InvalidProtocolBufferException e) {
+            System.out.print("Exception: ");
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
     @Test
     void addPeer() {
-        UnsignedTx tx = builder.addPeer("123.123.123.123", keys.publicKey()).build();
-        assertEquals(protoSize(tx), 180);
+        UnsignedTx tx = builder.addPeer("123.123.123.123:123", keys.publicKey()).build();
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void addSignatory() {
         UnsignedTx tx = builder.addSignatory("admin@test", keys.publicKey()).build();
-        assertEquals(protoSize(tx), 175);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void addAssetQuantity() {
         UnsignedTx tx = builder.addAssetQuantity("admin@test", "asset#domain", "12.345").build();
-        assertEquals(protoSize(tx), 164);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void removeSignatory() {
         UnsignedTx tx = builder.removeSignatory("admin@test", keys.publicKey()).build();
-        assertEquals(protoSize(tx), 175);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void createAccount() {
         UnsignedTx tx = builder.createAccount("admin", "domain", keys.publicKey()).build();
-        assertEquals(protoSize(tx), 178);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void createDomain() {
         UnsignedTx tx = builder.createDomain("domain", "role").build();
-        assertEquals(protoSize(tx), 143);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void setAccountQuorum() {
         UnsignedTx tx = builder.setAccountQuorum("admin@test", 123).build();
-        assertEquals(protoSize(tx), 143);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 
     @Test
     void transferAsset() {
         UnsignedTx tx = builder.transferAsset("from@test", "to@test", "asset#test", "description", "123.456").build();
-        assertEquals(protoSize(tx), 184);
+        assertTrue(checkProtoTx(proto(tx)));
     }
 }
