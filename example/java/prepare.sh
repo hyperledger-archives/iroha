eaf8ac8071f1a073302b8c85a844af9c6e0088fd
luckychess <toobwn@gmail.com>
2017-12-26 15:13:32 +0300

    Improve prepare.sh script.
    Signed-off-by: luckychess <toobwn@gmail.com>

--- example/java/prepare.sh
+++ example/java/prepare.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-
-cd $(dirname $0)
-
-# generate iroha lib
-if [[ $1 -eq 0 && ${IROHA_HOME} -eq 0 ]] ; then
-    echo 'Neither IROHA_HOME nor path to CMakeLists.txt is set'
-    exit 0
-fi
-cmake_path=${1,-${IROHA_HOME}}
-cmake -DSWIG_JAVA=ON $1
-make -j"$(getconf _NPROCESSORS_ONLN)" irohajava
+CURDIR="$(cd "$(dirname "$0")"; pwd)"
+IROHA_HOME="$(dirname $(dirname "${CURDIR}"))"
+cmake -H$IROHA_HOME -Bbuild -DSWIG_JAVA=ON;
+cmake --build build/ --target irohajava -- -j"$(getconf _NPROCESSORS_ONLN)"
