e6db057cbed91b68fde2b6e9e9f92d02f28eab2b
grimadas <bulat.nasrulin@gmail.com>
2018-01-14 11:40:42 +0800

    Add yac order fairness test
    Signed-off-by: grimadas <bulat.nasrulin@gmail.com>

--- test/module/irohad/consensus/yac/peer_orderer_test.cpp
+++ test/module/irohad/consensus/yac/peer_orderer_test.cpp
@@ -16,11 +16,10 @@
  */
 
 #include <iostream>
-#include <memory>
+#include <cmath>
+#include <unordered_map>
 #include <vector>
-
 #include "consensus/yac/impl/peer_orderer_impl.hpp"
-#include "module/irohad/ametsuchi/ametsuchi_mocks.hpp"
 #include "module/irohad/ametsuchi/ametsuchi_mocks.hpp"
 #include "module/irohad/consensus/yac/yac_mocks.hpp"
 
@@ -29,6 +28,8 @@ using namespace iroha::consensus::yac;
 
 using namespace std;
 using ::testing::Return;
+
+size_t N_PEERS = 4;
 
 class YacPeerOrdererTest : public ::testing::Test {
  public:
@@ -41,7 +42,7 @@ class YacPeerOrdererTest : public ::testing::Test {
 
   std::vector<iroha::model::Peer> peers = [] {
     std::vector<iroha::model::Peer> result;
-    for (size_t i = 1; i <= 4; ++i) {
+    for (size_t i = 1; i <= N_PEERS; ++i) {
       result.push_back(iroha::consensus::yac::mk_peer(std::to_string(i)));
     }
     return result;
@@ -81,4 +82,37 @@ TEST_F(YacPeerOrdererTest, PeerOrdererOrderingWhenInvokeFaillCase) {
   EXPECT_CALL(*wsv, getLedgerPeers()).WillOnce(Return(nonstd::nullopt));
   auto order = orderer.getOrdering(YacHash());
   ASSERT_EQ(order, nonstd::nullopt);
+}
+
+TEST_F(YacPeerOrdererTest, FairnessTest) {
+  // Calculate number of permutations of peers
+  size_t comb = std::tgamma(N_PEERS+1);
+  // Run experiments 30 times for each combination
+  size_t times = comb * 30;
+  std::unordered_map<std::string, int> histogram;
+  EXPECT_CALL(*wsv, getLedgerPeers())
+      .Times(times)
+      .WillRepeatedly(Return(peers));
+  for (size_t t = 1; t <= times; ++t) {
+    std::string hash_string =   std::to_string(t);
+    auto order = orderer.getOrdering(YacHash(hash_string, hash_string));
+    auto current_peers = order.value().getPeers();
+    std::string res = std::accumulate(current_peers.begin(),
+                                      current_peers.end(),
+                                      std::string(),
+                                      [](std::string res, const auto &peer) {
+                                        return res + " " + peer.address;
+                                      });
+    histogram[res]++;
+  }
+  ASSERT_EQ(comb, histogram.size());
+
+  // Output historgram, should output uniform distribution
+  std::cout << "The histogram output: " << std::endl;
+  std::for_each(histogram.begin(),
+                histogram.end(),
+                [](const std::pair<std::string, int> &element) {
+                  std::cout << element.first << " :: " << element.second
+                            << std::endl;
+                });
 }
