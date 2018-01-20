3655b2a723b7f441feb41612218618c5bf1e1b65
Kitsu <mail@kitsu.me>
2018-01-21 02:01:13 +0300

    Remove using namespace in conf loader
    Signed-off-by: Kitsu <mail@kitsu.me>

--- irohad/main/iroha_conf_loader.hpp
+++ irohad/main/iroha_conf_loader.hpp
@@ -44,7 +44,7 @@ namespace config_members {
  * @return rapidjson::Document is a parsed equivalent of that file
  */
 inline rapidjson::Document parse_iroha_config(const std::string &conf_path) {
-  using namespace assert_config;
+  namespace ac = assert_config;
   namespace mbr = config_members;
   rapidjson::Document doc;
   std::ifstream ifs_iroha(conf_path);
@@ -52,50 +52,56 @@ inline rapidjson::Document parse_iroha_config(const std::string &conf_path) {
   const std::string kStrType = "string";
   const std::string kUintType = "uint";
   doc.ParseStream(isw);
-  assert_fatal(not doc.HasParseError(), "JSON parse error: " + conf_path);
+  ac::assert_fatal(not doc.HasParseError(), "JSON parse error: " + conf_path);
 
-  assert_fatal(doc.HasMember(mbr::BlockStorePath),
-               no_member_error(mbr::BlockStorePath));
-  assert_fatal(doc[mbr::BlockStorePath].IsString(),
-               type_error(mbr::BlockStorePath, kStrType));
+  ac::assert_fatal(doc.HasMember(mbr::BlockStorePath),
+                   ac::no_member_error(mbr::BlockStorePath));
+  ac::assert_fatal(doc[mbr::BlockStorePath].IsString(),
+                   ac::type_error(mbr::BlockStorePath, kStrType));
 
-  assert_fatal(doc.HasMember(mbr::ToriiPort), no_member_error(mbr::ToriiPort));
-  assert_fatal(doc[mbr::ToriiPort].IsUint(),
-               type_error(mbr::ToriiPort, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::ToriiPort),
+                   ac::no_member_error(mbr::ToriiPort));
+  ac::assert_fatal(doc[mbr::ToriiPort].IsUint(),
+                   ac::type_error(mbr::ToriiPort, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::InternalPort),
-               no_member_error(mbr::InternalPort));
-  assert_fatal(doc[mbr::InternalPort].IsUint(),
-               type_error(mbr::InternalPort, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::InternalPort),
+                   ac::no_member_error(mbr::InternalPort));
+  ac::assert_fatal(doc[mbr::InternalPort].IsUint(),
+                   ac::type_error(mbr::InternalPort, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::PgOpt), no_member_error(mbr::PgOpt));
-  assert_fatal(doc[mbr::PgOpt].IsString(), type_error(mbr::PgOpt, kStrType));
+  ac::assert_fatal(doc.HasMember(mbr::PgOpt), ac::no_member_error(mbr::PgOpt));
+  ac::assert_fatal(doc[mbr::PgOpt].IsString(),
+                   ac::type_error(mbr::PgOpt, kStrType));
 
-  assert_fatal(doc.HasMember(mbr::RedisHost), no_member_error(mbr::RedisHost));
-  assert_fatal(doc[mbr::RedisHost].IsString(),
-               type_error(mbr::RedisHost, kStrType));
+  ac::assert_fatal(doc.HasMember(mbr::RedisHost),
+                   ac::no_member_error(mbr::RedisHost));
+  ac::assert_fatal(doc[mbr::RedisHost].IsString(),
+                   ac::type_error(mbr::RedisHost, kStrType));
 
-  assert_fatal(doc.HasMember(mbr::RedisPort), no_member_error(mbr::RedisPort));
-  assert_fatal(doc[mbr::RedisPort].IsUint(),
-               type_error(mbr::RedisPort, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::RedisPort),
+                   ac::no_member_error(mbr::RedisPort));
+  ac::assert_fatal(doc[mbr::RedisPort].IsUint(),
+                   ac::type_error(mbr::RedisPort, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::MaxProposalSize),
-               no_member_error(mbr::MaxProposalSize));
-  assert_fatal(doc[mbr::MaxProposalSize].IsUint(),
-               type_error(mbr::MaxProposalSize, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::MaxProposalSize),
+                   ac::no_member_error(mbr::MaxProposalSize));
+  ac::assert_fatal(doc[mbr::MaxProposalSize].IsUint(),
+                   ac::type_error(mbr::MaxProposalSize, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::ProposalDelay),
-               no_member_error(mbr::ProposalDelay));
-  assert_fatal(doc[mbr::ProposalDelay].IsUint(),
-               type_error(mbr::ProposalDelay, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::ProposalDelay),
+                   ac::no_member_error(mbr::ProposalDelay));
+  ac::assert_fatal(doc[mbr::ProposalDelay].IsUint(),
+                   ac::type_error(mbr::ProposalDelay, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::VoteDelay), no_member_error(mbr::VoteDelay));
-  assert_fatal(doc[mbr::VoteDelay].IsUint(),
-               type_error(mbr::VoteDelay, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::VoteDelay),
+                   ac::no_member_error(mbr::VoteDelay));
+  ac::assert_fatal(doc[mbr::VoteDelay].IsUint(),
+                   ac::type_error(mbr::VoteDelay, kUintType));
 
-  assert_fatal(doc.HasMember(mbr::LoadDelay), no_member_error(mbr::LoadDelay));
-  assert_fatal(doc[mbr::LoadDelay].IsUint(),
-               type_error(mbr::LoadDelay, kUintType));
+  ac::assert_fatal(doc.HasMember(mbr::LoadDelay),
+                   ac::no_member_error(mbr::LoadDelay));
+  ac::assert_fatal(doc[mbr::LoadDelay].IsUint(),
+                   ac::type_error(mbr::LoadDelay, kUintType));
   return doc;
 }
 
