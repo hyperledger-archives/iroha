7e6ea13bee8b352ac8967d72c442006d8b396b5a
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2018-01-23 21:45:49 +0300

    Remove unnecessary source files extensions
    Signed-off-by: Nikolay Yushkevich <n.yushkevich@hotmail.com>

--- cmake/clang-cxx-dev-tools.cmake
+++ cmake/clang-cxx-dev-tools.cmake
@@ -2,7 +2,7 @@
 # Get all project files
 file(GLOB_RECURSE
         ALL_CXX_SOURCE_FILES
-        *.[chi]pp *.[chi]xx *.cc *.hh *.ii *.[CHI]
+        *.[ch]pp
         )
 
 # Adding clang-format target if executable is found
