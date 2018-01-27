6cf32bf8b1ca6937a0ca08abec511038e295f3b7
Bogdan Vaneev <warchantua@gmail.com>
2018-01-27 17:10:15 +0300

    Remove unnecessary whoami
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- scripts/run-iroha-dev.sh
+++ scripts/run-iroha-dev.sh
@@ -3,10 +3,9 @@ CURDIR="$(cd "$(dirname "$0")"; pwd)"
 IROHA_HOME="$(dirname "${CURDIR}")"
 PROJECT=iroha${USERID}
 COMPOSE=${IROHA_HOME}/docker/docker-compose.yml
-USER=$(whoami)
 
-export G_ID=$(id -g $USER)
-export U_ID=$(id -u $USER)
+export G_ID=$(id -g)
+export U_ID=$(id -u)
 export COMPOSE_PROJECT_NAME=${PROJECT}
 
 docker-compose -f ${COMPOSE} up -d
