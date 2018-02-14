b74ef375cc4616b8c6e56854a46d9d41d17f43fd
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-02-14 12:04:22 +0300

    Add proto asset builder
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>

--- test/module/shared_model/builders/protobuf/common_objects/proto_asset_builder_test.cpp
+++ test/module/shared_model/builders/protobuf/common_objects/proto_asset_builder_test.cpp
@@ -17,18 +17,18 @@
 
 #include <gtest/gtest.h>
 
-#include "builders/protobuf/common_objects/proto_signature_builder.hpp"
+#include "builders/protobuf/common_objects/proto_asset_builder.hpp"
 
-TEST(ProtoSignatureBuilderTest, AllFieldsBuild) {
-  shared_model::proto::SignatureBuilder builder;
+TEST(ProtoAssetBuilderTest, AllFieldsBuild) {
+  shared_model::proto::AssetBuilder builder;
 
-  shared_model::interface::types::PubkeyType expected_key(std::string(32, '1'));
-  shared_model::interface::Signature::SignedType expected_signed("signed object");
+  auto expected_asset_id = "asset@coin";
+  auto expected_domain_id = "domain";
+  auto expected_precision = 2;
 
-  auto signature = builder.publicKey(expected_key).signedData(expected_signed)
-      .build();
+  auto asset = builder.assetId(expected_asset_id).domainId(expected_domain_id).precision(expected_precision).build();
 
-  EXPECT_EQ(signature.publicKey(), expected_key);
-  EXPECT_EQ(signature.signedData(), expected_signed);
+  EXPECT_EQ(asset.assetId(), expected_asset_id);
+  EXPECT_EQ(asset.domainId(), expected_domain_id);
+  EXPECT_EQ(asset.precision(), expected_precision);
 }
-
