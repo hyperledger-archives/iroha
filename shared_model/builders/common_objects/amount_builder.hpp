8e9f5031f4d80ee19ef7a802996bb8f2a4a39ee0
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-02-14 04:17:56 +0300

    Add amount builder
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>

---
+++ shared_model/builders/common_objects/amount_builder.hpp
@@ -0,0 +1,64 @@
+/**
+ * Copyright Soramitsu Co., Ltd. 2018 All Rights Reserved.
+ * http://soramitsu.co.jp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef IROHA_AMOUNT_BUILDER_HPP
+#define IROHA_AMOUNT_BUILDER_HPP
+
+#include "builders/common_objects/common.hpp"
+#include "interfaces/common_objects/amount.hpp"
+
+namespace shared_model {
+  namespace builder {
+    template <typename BuilderImpl, typename Validator>
+    class AmountBuilder {
+     public:
+      BuilderResult<shared_model::interface::Amount> build() {
+        auto amount = builder_.build();
+        shared_model::validation::ReasonsGroupType reasons(
+            "Amount Builder", shared_model::validation::GroupedReasons());
+        shared_model::validation::Answer answer;
+        validator_.validateAmount(reasons, amount);
+
+        if (!reasons.second.empty()) {
+          answer.addReason(std::move(reasons));
+          return iroha::expected::makeError(
+              std::make_shared<std::string>(answer.reason()));
+        }
+        std::shared_ptr<shared_model::interface::Amount> amount_ptr(amount.copy());
+        return iroha::expected::makeValue(
+            shared_model::detail::PolymorphicWrapper<
+                shared_model::interface::Amount>(amount_ptr));
+      }
+
+      AmountBuilder &intValue(const boost::multiprecision::uint256_t &value) {
+        builder_ = builder_.intValue(value);
+        return *this;
+      }
+
+      AmountBuilder &precision(const interface::types::PrecisionType &precision) {
+        builder_ = builder_.precision(precision);
+        return *this;
+      }
+
+     private:
+      Validator validator_;
+      BuilderImpl builder_;
+    };
+  }  // namespace builder
+}  // namespace shared_model
+
+#endif  // IROHA_AMOUNT_BUILDER_HPP
