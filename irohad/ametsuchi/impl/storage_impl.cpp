b9529bcfc90b680dc6661dd7dd6f4826ceafa0bd
Nikita Alekseev <n.alekseev2104@gmail.com>
2018-02-26 11:28:33 +0300

    Fix formatting exception in storage and ordering service state
    Signed-off-by: Nikita Alekseev <n.alekseev2104@gmail.com>

--- irohad/ametsuchi/impl/storage_impl.cpp
+++ irohad/ametsuchi/impl/storage_impl.cpp
@@ -189,7 +189,7 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
       auto block_store = FlatFile::create(block_store_dir);
       if (not block_store) {
         return expected::makeError(
-            (boost::format("Cannot create block store in {}") % block_store_dir)
+            (boost::format("Cannot create block store in %s") % block_store_dir)
                 .str());
       }
       log_->info("block store created");
