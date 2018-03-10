4080e4cbafae83e0237e2daec6615575ab429167
Dumitru <dimasavva17@gmail.com>
2018-03-10 13:56:36 +0300

    Fix/libpqxx version (#1063)
    * Set pqxx to the latest release
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>
    
    * Comment version
    
    Signed-off-by: Dumitru <dimasavva17@gmail.com>

--- cmake/Modules/Findpqxx.cmake
+++ cmake/Modules/Findpqxx.cmake
@@ -12,7 +12,7 @@ find_package_handle_standard_args(pqxx DEFAULT_MSG
     )
 
 set(URL https://github.com/jtv/libpqxx.git)
-set(VERSION 5b17abce5ac2b1a2f8278718405b7ade8bb30ae9)
+set(VERSION c4d4f4b4e6ecaf85de770a030f3a1cdc1b3a79ae) # 6.1.0
 set_target_description(pqxx "C++ bindings for postgres client library" ${URL} ${VERSION})
 
 if (NOT pqxx_FOUND)
