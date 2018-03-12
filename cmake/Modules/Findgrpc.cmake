98fc6ba943b94826a45d32c80c7b028ff2669638
Bogdan <bogdan@soramitsu.co.jp>
2018-03-12 11:54:41 +0200

    Fix docker develop dockerfile (#1064)
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/Modules/Findgrpc.cmake
+++ cmake/Modules/Findgrpc.cmake
@@ -1,6 +1,5 @@
 add_library(grpc UNKNOWN IMPORTED)
 add_library(grpc++ UNKNOWN IMPORTED)
-add_library(grpc++_reflection UNKNOWN IMPORTED)
 add_library(gpr UNKNOWN IMPORTED)
 add_executable(grpc_cpp_plugin IMPORTED)
 
@@ -13,9 +12,6 @@ mark_as_advanced(grpc_LIBRARY)
 find_library(grpc_grpc++_LIBRARY grpc++)
 mark_as_advanced(grpc_grpc++_LIBRARY)
 
-find_library(grpc_grpc++_reflection_LIBRARY grpc++_reflection)
-mark_as_advanced(grpc_grpc++_reflection_LIBRARY)
-
 find_library(gpr_LIBRARY gpr)
 mark_as_advanced(gpr_LIBRARY)
 
@@ -26,7 +22,6 @@ find_package_handle_standard_args(grpc DEFAULT_MSG
     grpc_LIBRARY
     grpc_INCLUDE_DIR
     gpr_LIBRARY
-    grpc_grpc++_reflection_LIBRARY
     grpc_CPP_PLUGIN
     )
 
@@ -39,12 +34,18 @@ if (NOT grpc_FOUND)
   externalproject_add(grpc_grpc
       GIT_REPOSITORY ${URL}
       GIT_TAG        ${VERSION}
-      CMAKE_ARGS -DgRPC_PROTOBUF_PROVIDER=package -DgRPC_PROTOBUF_PACKAGE_TYPE=CONFIG -DProtobuf_DIR=${EP_PREFIX}/src/google_protobuf-build/lib/cmake/protobuf -DgRPC_ZLIB_PROVIDER=package -DBUILD_SHARED_LIBS=ON
+      CMAKE_ARGS
+        -DgRPC_PROTOBUF_PROVIDER=package
+        -DgRPC_PROTOBUF_PACKAGE_TYPE=CONFIG
+        -DProtobuf_DIR=${EP_PREFIX}/src/google_protobuf-build/lib/cmake/protobuf
+        -DgRPC_ZLIB_PROVIDER=package
+        -DBUILD_SHARED_LIBS=ON
       PATCH_COMMAND ${GIT_EXECUTABLE} apply ${PROJECT_SOURCE_DIR}/patch/fix-protobuf-package-include.patch || true
-      BUILD_BYPRODUCTS ${EP_PREFIX}/src/grpc_grpc-build/grpc_cpp_plugin
-                       ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}grpc${CMAKE_SHARED_LIBRARY_SUFFIX}
-                       ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}grpc++${CMAKE_SHARED_LIBRARY_SUFFIX}
-                       ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}grpc++_reflection${CMAKE_SHARED_LIBRARY_SUFFIX}
+      BUILD_BYPRODUCTS
+        ${EP_PREFIX}/src/grpc_grpc-build/grpc_cpp_plugin
+        ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}gpr${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}grpc${CMAKE_SHARED_LIBRARY_SUFFIX}
+        ${EP_PREFIX}/src/grpc_grpc-build/${CMAKE_SHARED_LIBRARY_PREFIX}grpc++${CMAKE_SHARED_LIBRARY_SUFFIX}
       INSTALL_COMMAND "" # remove install step
       TEST_COMMAND "" # remove test step
       UPDATE_COMMAND "" # remove update step
@@ -53,7 +54,6 @@ if (NOT grpc_FOUND)
   set(grpc_INCLUDE_DIR ${source_dir}/include)
   set(grpc_LIBRARY ${binary_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}grpc${CMAKE_SHARED_LIBRARY_SUFFIX})
   set(grpc_grpc++_LIBRARY ${binary_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}grpc++${CMAKE_SHARED_LIBRARY_SUFFIX})
-  set(grpc_grpc++_reflection_LIBRARY ${binary_dir}/${CMAKE_SHARED_LIBRARY_PREFIX}grpc++_reflection${CMAKE_SHARED_LIBRARY_SUFFIX})
   set(grpc_CPP_PLUGIN ${binary_dir}/grpc_cpp_plugin)
   file(MAKE_DIRECTORY ${grpc_INCLUDE_DIR})
   link_directories(${binary_dir})
@@ -61,7 +61,6 @@ if (NOT grpc_FOUND)
   add_dependencies(grpc_grpc protobuf)
   add_dependencies(grpc grpc_grpc)
   add_dependencies(grpc++ grpc_grpc)
-  add_dependencies(grpc++_reflection grpc_grpc)
   add_dependencies(grpc_cpp_plugin grpc_grpc protoc)
 endif ()
 
@@ -75,12 +74,6 @@ set_target_properties(grpc++ PROPERTIES
     INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
     INTERFACE_LINK_LIBRARIES grpc
     IMPORTED_LOCATION ${grpc_grpc++_LIBRARY}
-    )
-
-set_target_properties(grpc++_reflection PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIR}
-    INTERFACE_LINK_LIBRARIES grpc++
-    IMPORTED_LOCATION ${grpc_grpc++_reflection_LIBRARY}
     )
 
 set_target_properties(grpc_cpp_plugin PROPERTIES
