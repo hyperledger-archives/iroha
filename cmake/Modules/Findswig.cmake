e3f9ab8818faf36ec56eea9af47b8c96e173e5a0
luckychess <toobwn@gmail.com>
2018-03-26 19:41:26 +0300

    Swig fixes (#1133):
    - fix path to swig patch
    - use tarball instead of git for swig url
    
    Signed-off-by: luckychess <toobwn@gmail.com>

--- cmake/Modules/Findswig.cmake
+++ cmake/Modules/Findswig.cmake
@@ -9,15 +9,12 @@ find_package_handle_standard_args(SWIG DEFAULT_MSG
 if(NOT SWIG_EXECUTABLE)
   find_package(Git REQUIRED)
 
-  set(URL https://github.com/swig/swig)
-  set(VERSION fbeb566014a1d320df972aef965daf042db7db36) # Tag origin/rel-3.0.12
-  set_target_description(swig "Simplified Wrapper and Interface Generator (SWIG)" ${URL} ${VERSION})
+  set(URL https://prdownloads.sourceforge.net/swig/swig-3.0.12.tar.gz)
+  set_target_description(swig "Simplified Wrapper and Interface Generator (SWIG)" ${URL} 3.0.12)
 
   ExternalProject_Add(swig_swig
-      GIT_REPOSITORY ${URL}
-      GIT_TAG        ${VERSION}
-      GIT_PROGRESS   ON
-      PATCH_COMMAND ${GIT_EXECUTABLE} apply ${PROJECT_SOURCE_DIR}/patch/add-nodejs8-support-to-swig.patch || true
+      URL ${URL}
+      PATCH_COMMAND patch -p1 < ${PROJECT_SOURCE_DIR}/../patch/add-nodejs8-support-to-swig.patch || true
       # We should install SWIG to properly access SWIG lib
       CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --without-pcre --prefix=${EP_PREFIX}/src/swig_swig
       BUILD_IN_SOURCE ON
