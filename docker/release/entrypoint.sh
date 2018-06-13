098ec30e9611c07211f9698debbc99be4f21d492
Artyom Bakhtin <a@bakhtin.net>
2018-06-13 17:10:10 +0300

    change Dockerfile to use config.docker (#1426)
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo key=$KEY
 echo $PWD
-irohad --genesis_block genesis.block --config config.sample --keypair_name $KEY
+irohad --genesis_block genesis.block --config config.docker --keypair_name $KEY
