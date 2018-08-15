c29e2390dda470e5c5e332dd39d5dba4a13815d4
Carver182 <carver182@mail.ru>
2018-08-15 11:17:32 +0300

    Add get roles test (#1636)
    This test checks whether the user can query list of roles
    
    Signed-off-by: Carver182 <carver182@mail.ru>

--- test/integration/acceptance/get_roles_test.cpp
+++ test/integration/acceptance/get_roles_test.cpp
@@ -4,83 +4,79 @@
  */
 
 #include <gtest/gtest.h>
-#include "integration/acceptance/acceptance_fixture.hpp"
-#include "framework/integration_framework/integration_test_framework.hpp"
 #include "backend/protobuf/transaction.hpp"
+#include "framework/integration_framework/integration_test_framework.hpp"
 #include "framework/specified_visitor.hpp"
+#include "integration/acceptance/acceptance_fixture.hpp"
 #include "interfaces/permissions.hpp"
-
 
 using namespace integration_framework;
 using namespace shared_model;
 
-class GetRoles : public AcceptanceFixture {
-public:
-//    auto makeUserWithPerms(const interface::RolePermissionSet &perms = {
-//            interface::permissions::Role::kGetRoles}) {
-//        return AcceptanceFixture::makeUserWithPerms(perms);
-//    }
-
-};
-
 /**
  * @given a user with CanGetRoles permission
  * @when execute query with getRoles command
- * @then there is should be no exception
+ * @then the query returns list of roles
  */
-TEST_F(GetRoles, CanGetRoles) {
-    auto checkQuery = [](auto &queryResponse) {
-        ASSERT_NO_THROW(boost::apply_visitor(
-                framework::SpecifiedVisitor<shared_model::interface::RolesResponse>(), queryResponse.get()));
-    };
+TEST_F(AcceptanceFixture, CanGetRoles) {
+  auto checkQuery = [](auto &queryResponse) {
+    ASSERT_NO_THROW(boost::apply_visitor(
+        framework::SpecifiedVisitor<shared_model::interface::RolesResponse>(),
+        queryResponse.get()));
+  };
 
-    auto query = TestUnsignedQueryBuilder()
-            .createdTime(iroha::time::now())
-            .creatorAccountId(kUserId)
-            .queryCounter(1)
-            .getRoles()
-            .build()
-            .signAndAddSignature(kUserKeypair)
-            .finish();
+  auto query = TestUnsignedQueryBuilder()
+                   .createdTime(iroha::time::now())
+                   .creatorAccountId(kUserId)
+                   .queryCounter(1)
+                   .getRoles()
+                   .build()
+                   .signAndAddSignature(kUserKeypair)
+                   .finish();
 
-    IntegrationTestFramework(1)
-            .setInitialState(kAdminKeypair)
-            .sendTx(makeUserWithPerms({shared_model::interface::permissions::Role::kGetRoles}))
-            .skipProposal()
-            .checkBlock([](auto &block) {
-                ASSERT_EQ(boost::size(block->transactions()), 1);
-            })
-            .sendQuery(query, checkQuery);
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms(
+          {shared_model::interface::permissions::Role::kGetRoles}))
+      .skipProposal()
+      .checkBlock(
+          [](auto &block) { ASSERT_EQ(boost::size(block->transactions()), 1); })
+      .sendQuery(query, checkQuery);
 }
 
 /**
  * @given a user without CanGetRoles permission
  * @when execute query with getRoles command
- * @then there is should be an exception
+ * @then there is no way to to get roles due to user hasn't permissions enough
  */
-TEST_F(GetRoles, CanNotGetRoles) {
-    auto checkQuery = [](auto &queryResponse) {
-        ASSERT_ANY_THROW(boost::apply_visitor(
-                framework::SpecifiedVisitor<shared_model::interface::RolesResponse>(), queryResponse.get()));
-    };
+TEST_F(AcceptanceFixture, CanNotGetRoles) {
+  auto checkQuery = [](auto &queryResponse) {
+    ASSERT_NO_THROW({
+      boost::apply_visitor(
+          framework::SpecifiedVisitor<
+              shared_model::interface::StatefulFailedErrorResponse>(),
+          boost::apply_visitor(
+              framework::SpecifiedVisitor<
+                  shared_model::interface::ErrorQueryResponse>(),
+              queryResponse.get())
+              .get());
+    });
+  };
 
-    auto query = TestUnsignedQueryBuilder()
-            .createdTime(iroha::time::now())
-            .creatorAccountId(kUserId)
-            .queryCounter(1)
-            .getRoles()
-            .build()
-            .signAndAddSignature(kUserKeypair)
-            .finish();
+  auto query = TestUnsignedQueryBuilder()
+                   .createdTime(iroha::time::now())
+                   .creatorAccountId(kUserId)
+                   .queryCounter(1)
+                   .getRoles()
+                   .build()
+                   .signAndAddSignature(kUserKeypair)
+                   .finish();
 
-    IntegrationTestFramework(1)
-            .setInitialState(kAdminKeypair)
-            .sendTx(makeUserWithPerms({}))
-            .skipProposal()
-            .checkBlock([](auto &block) {
-                ASSERT_EQ(boost::size(block->transactions()), 1);
-            })
-            .sendQuery(query, checkQuery);
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms({}))
+      .skipProposal()
+      .checkBlock(
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); })
+      .sendQuery(query, checkQuery);
 }
-
-
