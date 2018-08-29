82184dd2a573d3f05031b192795a29f6ec1d4605
Igor Egorov <igor@soramitsu.co.jp>
2018-08-29 15:15:55 +0300

    Add SendTx and SendQuery system tests (#1673)
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- libs/crypto/keys_manager.hpp
+++ libs/crypto/keys_manager.hpp
@@ -1,18 +1,6 @@
 /**
- * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
- * http://soramitsu.co.jp
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef IROHA_KEYS_MANAGER_HPP
@@ -21,12 +9,7 @@
 #include <string>
 
 #include <boost/optional.hpp>
-
-namespace shared_model {
-  namespace crypto {
-    class Keypair;
-  }
-}
+#include "cryptography/keypair.hpp"
 
 namespace iroha {
   /**
@@ -37,32 +20,32 @@ namespace iroha {
     virtual ~KeysManager() = default;
 
     /**
-     * Create a new keypair and store it as is on disk
-     * @return false if create account failed
+     * Create keys of a new keypair and store them on disk
+     * @return false if keys creation was failed
      */
     virtual bool createKeys() = 0;
 
     /**
-     * Create keys a new keypair and store it encrypted on disk
-     * @param pass_phrase is a password for the keys
-     * @return false if create account failed
+     * Create keys of a new keypair and store it encrypted on disk
+     * @param pass_phrase is used for private key encryption
+     * @return false if keys creation was failed
      */
     virtual bool createKeys(const std::string &pass_phrase) = 0;
 
     /**
-     * Load plain-text keys associated with the manager, then validate loaded
-     * keypair by signing and verifying signature of test message
-     * @return nullopt if no keypair found locally, or verification failure;
-     *         related keypair otherwise
+     * Load keys associated with the manager, then validate loaded keypair by
+     * signing and verifying the signature of a test message
+     * @return nullopt if no keypair found locally, or in case of verification
+     *         failure. Otherwise - the keypair will be returned
      */
     virtual boost::optional<shared_model::crypto::Keypair> loadKeys() = 0;
 
     /**
      * Load encrypted keys associated with the manager, then validate loaded
-     * keypair by signing and verifying signature of test message
+     * keypair by signing and verifying the signature of a test message
      * @param pass_phrase is a password for decryption
-     * @return nullopt if no keypair found locally, or verification failure;
-     *         related keypair otherwise
+     * @return nullopt if no keypair found locally, or in case of verification
+     *         failure. Otherwise - the keypair will be returned
      */
     virtual boost::optional<shared_model::crypto::Keypair> loadKeys(
         const std::string &pass_phrase) = 0;
--- test/system/irohad_test.cpp
+++ test/system/irohad_test.cpp
@@ -1,33 +1,28 @@
 /**
- * Copyright Soramitsu Co., Ltd. 2018 All Rights Reserved.
- * http://soramitsu.co.jp
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <gtest/gtest.h>
 #include <rapidjson/document.h>
 #include <rapidjson/prettywriter.h>
 #include <rapidjson/stringbuffer.h>
+#include <soci/postgresql/soci-postgresql.h>
+#include <soci/soci.h>
 #include <boost/filesystem.hpp>
 #include <boost/optional.hpp>
 #include <boost/process.hpp>
-#include <soci/soci.h>
-#include <soci/postgresql/soci-postgresql.h>
 
+#include "backend/protobuf/query_responses/proto_query_response.hpp"
 #include "common/files.hpp"
 #include "common/types.hpp"
+#include "crypto/keys_manager_impl.hpp"
+#include "framework/specified_visitor.hpp"
+#include "integration/acceptance/acceptance_fixture.hpp"
+#include "interfaces/query_responses/roles_response.hpp"
 #include "main/iroha_conf_loader.hpp"
+#include "torii/command_client.hpp"
+#include "torii/query_client.hpp"
 
 // workaround for redefining -WERROR problem
 #undef RAPIDJSON_HAS_STDSTRING
@@ -39,10 +34,11 @@ using namespace boost::filesystem;
 using namespace std::chrono_literals;
 using iroha::operator|;
 
-class IrohadTest : public testing::Test {
+class IrohadTest : public AcceptanceFixture {
  public:
+  IrohadTest() : kAddress("127.0.0.1"), kPort(50051) {}
+
   void SetUp() override {
-    timeout = 1s;
     setPaths();
     auto config = parse_iroha_config(path_config_.string());
     blockstore_path_ = (boost::filesystem::temp_directory_path()
@@ -66,7 +62,17 @@ class IrohadTest : public testing::Test {
     copy_file.write(config_copy_string.data(), config_copy_string.size());
   }
 
+  void launchIroha() {
+    iroha_process_.emplace(irohad_executable.string() + setDefaultParams());
+    std::this_thread::sleep_for(kTimeout);
+    ASSERT_TRUE(iroha_process_->running());
+  }
+
   void TearDown() override {
+    if (iroha_process_) {
+      iroha_process_->terminate();
+    }
+
     iroha::remove_dir_contents(blockstore_path_);
     dropPostgres();
     boost::filesystem::remove(config_copy_);
@@ -122,9 +128,26 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
 
  public:
   boost::filesystem::path irohad_executable;
-  std::chrono::milliseconds timeout = 1s;
+  const std::chrono::milliseconds kTimeout = std::chrono::seconds(1);
+  const std::string kAddress;
+  const uint16_t kPort;
 
- private:
+  boost::optional<child> iroha_process_;
+
+  /**
+   * Command client resubscription settings
+   *
+   * The do-while loop imitates client resubscription to the stream. Stream
+   * "expiration" is a valid designed case (see pr #1615 for the details).
+   *
+   * The number of attempts (5) is a magic constant here. The idea behind this
+   * number is the following: five resubscription with 3 seconds timeout is
+   * usually enough to pass the test; if not - most likely there is another bug.
+   */
+  const uint32_t resubscribe_attempts = 5;
+  const std::chrono::seconds resubscribe_timeout = std::chrono::seconds(3);
+
+ protected:
   boost::filesystem::path path_irohad_;
   boost::filesystem::path path_example_;
   boost::filesystem::path path_config_;
@@ -135,15 +158,69 @@ DROP TABLE IF EXISTS index_by_id_height_asset;
   std::string config_copy_;
 };
 
-/*
+/**
  * @given path to irohad executable and paths to files irohad is needed to be
  * run (config, genesis block, keypair)
  * @when run irohad with all parameters it needs to operate as a full node
  * @then irohad should be started and running until timeout expired
  */
 TEST_F(IrohadTest, RunIrohad) {
-  child c(irohad_executable.string() + setDefaultParams());
-  std::this_thread::sleep_for(timeout);
-  ASSERT_TRUE(c.running());
-  c.terminate();
+  launchIroha();
+}
+
+/**
+ * Test verifies that a transaction can be sent to running iroha and committed
+ * @given running Iroha
+ * @when a client sends a transaction to Iroha
+ * @then the transaction is committed
+ */
+TEST_F(IrohadTest, SendTx) {
+  launchIroha();
+  auto key_manager = iroha::KeysManagerImpl(kAdminId, path_example_);
+  auto key_pair = key_manager.loadKeys();
+  ASSERT_TRUE(key_pair);
+
+  iroha::protocol::TxStatusRequest tx_request;
+  iroha::protocol::ToriiResponse torii_response;
+
+  auto tx =
+      complete(baseTx(kAdminId).setAccountQuorum(kAdminId, 1), key_pair.get());
+  tx_request.set_tx_hash(shared_model::crypto::toBinaryString(tx.hash()));
+
+  auto client = torii::CommandSyncClient(kAddress, kPort);
+  client.Torii(tx.getTransport());
+
+  auto resub_counter(resubscribe_attempts);
+  const auto committed_status = iroha::protocol::TxStatus::COMMITTED;
+  do {
+    std::this_thread::sleep_for(resubscribe_timeout);
+    client.Status(tx_request, torii_response);
+  } while (torii_response.tx_status() != committed_status and --resub_counter);
+
+  ASSERT_EQ(torii_response.tx_status(), committed_status);
+}
+
+/**
+ * Test verifies that a query can be sent to and served by running Iroha
+ * @given running Iroha
+ * @when a client sends a query to Iroha
+ * @then the query is served and query response is received
+ */
+TEST_F(IrohadTest, SendQuery) {
+  launchIroha();
+  auto key_manager = iroha::KeysManagerImpl(kAdminId, path_example_);
+  auto key_pair = key_manager.loadKeys();
+  ASSERT_TRUE(key_pair);
+
+  iroha::protocol::QueryResponse response;
+  auto query = complete(baseQry(kAdminId).getRoles(), key_pair.get());
+  auto client = torii_utils::QuerySyncClient(kAddress, kPort);
+  client.Find(query.getTransport(), response);
+  auto resp = shared_model::proto::QueryResponse(response);
+
+  ASSERT_NO_THROW({
+    boost::apply_visitor(
+        framework::SpecifiedVisitor<shared_model::interface::RolesResponse>(),
+        resp.get());
+  });
 }
