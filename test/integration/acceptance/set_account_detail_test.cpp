4309f5baafd639682f4b572fa06d0b16b6719ce9
Kitsu <mail@kitsu.me>
2018-09-18 22:15:57 +0300

    Add more acceptance tests for SetAccountDetail (#1723)
    Signed-off-by: Kitsu <mail@kitsu.me>

--- test/integration/acceptance/set_account_detail_test.cpp
+++ test/integration/acceptance/set_account_detail_test.cpp
@@ -49,9 +49,10 @@ class SetAccountDetail : public AcceptanceFixture {
 };
 
 /**
+ * C274
  * @given a user without can_set_detail permission
  * @when execute tx with SetAccountDetail command aimed to the user
- * @then there is the tx in proposal
+ * @then there is the tx in block
  */
 TEST_F(SetAccountDetail, Self) {
   IntegrationTestFramework(1)
@@ -65,6 +66,25 @@ TEST_F(SetAccountDetail, Self) {
 }
 
 /**
+ * C273
+ * @given a user with required permission
+ * @when execute tx with SetAccountDetail command with inexistent user
+ * @then there is no tx in block
+ */
+TEST_F(SetAccountDetail, NonExistentUser) {
+  const std::string kInexistent = "inexistent@" + kDomain;
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms({interface::permissions::Role::kSetDetail}))
+      .skipProposal()
+      .skipBlock()
+      .sendTxAwait(
+          complete(baseTx(kInexistent, kKey, kValue)),
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 0); });
+}
+
+/**
+ * C280
  * @given a pair of users and first one without permissions
  * @when the first one tries to use SetAccountDetail on the second
  * @then there is an empty verified proposal
@@ -93,7 +113,7 @@ TEST_F(SetAccountDetail, WithoutNoPerm) {
 /**
  * @given a pair of users and first one with can_set_detail perm
  * @when the first one tries to use SetAccountDetail on the second
- * @then there is the tx in proposal
+ * @then there is the tx in block
  */
 TEST_F(SetAccountDetail, WithPerm) {
   auto second_user_tx = makeSecondUser();
@@ -113,6 +133,7 @@ TEST_F(SetAccountDetail, WithPerm) {
 }
 
 /**
+ * C275
  * @given a pair of users
  *        @and second has been granted can_set_my_detail from the first
  * @when the first one tries to use SetAccountDetail on the second
@@ -146,4 +167,75 @@ TEST_F(SetAccountDetail, WithGrantablePerm) {
       .sendTxAwait(set_detail_cmd, [](auto &block) {
         ASSERT_EQ(block->transactions().size(), 1);
       });
+}
+
+/**
+ * C276
+ * @given a user with required permission
+ * @when execute tx with SetAccountDetail command with max key
+ * @then there is the tx in block
+ */
+TEST_F(SetAccountDetail, BigPossibleKey) {
+  const std::string kBigKey = std::string(64, 'a');
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms())
+      .skipProposal()
+      .skipBlock()
+      .sendTxAwait(complete(baseTx(kUserId, kBigKey, kValue)), [](auto &block) {
+        ASSERT_EQ(block->transactions().size(), 1);
+      });
+}
+
+/**
+ * C277
+ * @given a user with required permission
+ * @when execute tx with SetAccountDetail command with empty key
+ * @then there is no tx in block
+ */
+TEST_F(SetAccountDetail, EmptyKey) {
+  const std::string kEmptyKey = "";
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms())
+      .skipProposal()
+      .skipBlock()
+      .sendTx(complete(baseTx(kUserId, kEmptyKey, kValue)),
+              checkStatelessInvalid);
+}
+
+/**
+ * C278
+ * @given a user with required permission
+ * @when execute tx with SetAccountDetail command with empty value
+ * @then there is no tx in block
+ */
+TEST_F(SetAccountDetail, EmptyValue) {
+  const std::string kEmptyValue = "";
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms())
+      .skipProposal()
+      .skipBlock()
+      .sendTxAwait(
+          complete(baseTx(kUserId, kKey, kEmptyValue)),
+          [](auto &block) { ASSERT_EQ(block->transactions().size(), 1); });
+}
+
+/**
+ * C279
+ * @given a user with required permission
+ * @when execute tx with SetAccountDetail command with huge both key and value
+ * @then there is no tx in block
+ */
+TEST_F(SetAccountDetail, HugeKeyValue) {
+  const std::string kHugeKey = std::string(10000, 'a');
+  const std::string kHugeValue = std::string(10000, 'b');
+  IntegrationTestFramework(1)
+      .setInitialState(kAdminKeypair)
+      .sendTx(makeUserWithPerms())
+      .skipProposal()
+      .skipBlock()
+      .sendTx(complete(baseTx(kUserId, kHugeKey, kHugeValue)),
+              checkStatelessInvalid);
 }
