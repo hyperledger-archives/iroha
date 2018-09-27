b6a95473c4f8b2e5352c5653f2356b64ecdfc831
Artyom Bakhtin <a@bakhtin.net>
2018-09-27 18:18:38 +0300

    wait for postgres (#1745)
    - add wait-for-it.sh script
    - modify entrypoint.sh to use wait-for-it.sh
    - introduces new variables IROHA_POSTGRES_HOST and IROHA_POSTGRES_PORT
    
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- docker/release/entrypoint.sh
+++ docker/release/entrypoint.sh
@@ -2,4 +2,12 @@
 
 echo key=$KEY
 echo $PWD
+if [ -n "$IROHA_POSTGRES_HOST" ]; then
+  echo "NOTE: IROHA_POSTGRES_HOST should match 'host' option in config file"
+  PG_PORT=${IROHA_POSTGRES_PORT:-5432}
+  /wait-for-it.sh -h $IROHA_POSTGRES_HOST -p $PG_PORT -t 30 -- true
+else
+  echo "WARNING: IROHA_POSTGRES_HOST is not defined.
+    Do not wait for Postgres to become ready. Iroha may fail to start up"
+fi
 irohad --genesis_block genesis.block --config config.docker --keypair_name $KEY
