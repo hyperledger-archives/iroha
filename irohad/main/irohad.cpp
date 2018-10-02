2211d9ab46ba31c173f83a6806afc2da2755940e
Kitsu <mail@kitsu.me>
2018-10-03 00:53:03 +0300

    Add irohad verbosity flag (#1735)
    Signed-off-by: Kitsu <mail@kitsu.me>

--- irohad/main/irohad.cpp
+++ irohad/main/irohad.cpp
@@ -78,9 +78,24 @@ DEFINE_validator(keypair_name, &validate_keypair_name);
  */
 DEFINE_bool(overwrite_ledger, false, "Overwrite ledger data if existing");
 
+static bool validateVerbosity(const char *flagname, int32_t val) {
+  if (val >= 0 && val <= 6)
+    return true;
+
+  std::cout << "Invalid value for " << flagname << ": should be in range [0, 6]"
+            << std::endl;
+  return false;
+}
+
+/// Verbosity flag for spdlog configuration
+DEFINE_int32(verbosity, spdlog::level::info, "Log verbosity");
+DEFINE_validator(verbosity, validateVerbosity);
+
 std::promise<void> exit_requested;
 
 int main(int argc, char *argv[]) {
+  spdlog::set_level(spdlog::level::level_enum(FLAGS_verbosity));
+
   auto log = logger::log("MAIN");
   log->info("start");
 
