1db35ece917c43983d524f867435d09409d87269
Andrei Lebedev <lebdron@gmail.com>
2018-10-10 19:16:43 +0300

    Move string builder implementation to cpp (#1765)
    * Replace for_each with range-based for
    
    Signed-off-by: Andrei Lebedev <lebdron@gmail.com>

--- shared_model/utils/string_builder.hpp
+++ shared_model/utils/string_builder.hpp
@@ -1,24 +1,11 @@
 /**
- * Copyright Soramitsu Co., Ltd. 2017 All Rights Reserved.
- * http://soramitsu.co.jp
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef IROHA_SHARED_MODEL_STRING_BUILDER_HPP
 #define IROHA_SHARED_MODEL_STRING_BUILDER_HPP
 
-#include <algorithm>
 #include <string>
 
 namespace shared_model {
@@ -32,29 +19,17 @@ namespace shared_model {
        * Initializes new string with a provided name
        * @param name - name to initialize
        */
-      PrettyStringBuilder &init(const std::string &name) {
-        result_.append(name);
-        result_.append(initSeparator);
-        result_.append(spaceSeparator);
-        result_.append(beginBlockMarker);
-        return *this;
-      }
+      PrettyStringBuilder &init(const std::string &name);
 
       /**
        * Inserts new level marker
        */
-      PrettyStringBuilder &insertLevel() {
-        result_.append(beginBlockMarker);
-        return *this;
-      }
+      PrettyStringBuilder &insertLevel();
 
       /**
        * Closes new level marker
        */
-      PrettyStringBuilder &removeLevel() {
-        result_.append(endBlockMarker);
-        return *this;
-      }
+      PrettyStringBuilder &removeLevel();
 
       /**
        * Appends new field to string as a "name=value" pair
@@ -62,24 +37,13 @@ namespace shared_model {
        * @param value - field value
        */
       PrettyStringBuilder &append(const std::string &name,
-                                  const std::string &value) {
-        result_.append(name);
-        result_.append(keyValueSeparator);
-        result_.append(value);
-        result_.append(singleFieldsSeparator);
-        result_.append(spaceSeparator);
-        return *this;
-      }
+                                  const std::string &value);
 
       /**
        * Appends new single value to string
        * @param value - value to append
        */
-      PrettyStringBuilder &append(const std::string &value) {
-        result_.append(value);
-        result_.append(spaceSeparator);
-        return *this;
-      }
+      PrettyStringBuilder &append(const std::string &value);
 
       /**
        * Appends a new collection to string
@@ -91,9 +55,9 @@ namespace shared_model {
       template <typename Collection, typename Transform>
       PrettyStringBuilder &appendAll(Collection &&c, Transform &&t) {
         insertLevel();
-        std::for_each(c.begin(), c.end(), [this, &t](auto &val) {
-          this->append(t(val));
-        });
+        for (auto &val : c) {
+          append(t(val));
+        }
         removeLevel();
         return *this;
       }
@@ -102,10 +66,7 @@ namespace shared_model {
        * Finalizes appending and returns constructed string.
        * @return resulted string
        */
-      std::string finalize() {
-        result_.append(endBlockMarker);
-        return result_;
-      }
+      std::string finalize();
 
      private:
       std::string result_;
