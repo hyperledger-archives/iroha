84cf8e62cf3ef93f052726e4b22ee01c5f731438
Mikhail Boldyrev <miboldyrev@gmail.com>
2018-10-24 14:53:38 +0300

    fake peer ignores yac states with many votes
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/framework/integration_framework/fake_peer/fake_peer.cpp
+++ test/framework/integration_framework/fake_peer/fake_peer.cpp
@@ -135,6 +135,14 @@ namespace integration_framework {
 
   void FakePeer::voteForTheSame(const YacStateMessage &incoming_votes) {
     using iroha::consensus::yac::VoteMessage;
+    log_->debug("Got a YAC state message with {} votes.",
+                incoming_votes->size());
+    if (incoming_votes->size() > 1) {
+      // TODO mboldyrev 24/10/2018: rework ignoring states for accepted commits
+      log_->debug("Ignoring state with multiple votes, "
+          "because it probably refers to an accepted commit.");
+      return;
+    }
     std::vector<VoteMessage> my_votes;
     my_votes.reserve(incoming_votes->size());
     std::transform(
