030e3bb295b478e8d23f6010df5738fbd28dd1cc
Mikhail Boldyrev <miboldyrev@gmail.com>
2018-12-17 19:14:51 +0300

    fix macro args for clang
    Signed-off-by: Mikhail Boldyrev <miboldyrev@gmail.com>

--- test/module/irohad/consensus/yac/supermajority_checker_test.cpp
+++ test/module/irohad/consensus/yac/supermajority_checker_test.cpp
@@ -67,11 +67,15 @@ using BftSupermajorityCheckerTest = SupermajorityCheckerTest;
 INSTANTIATE_TEST_CASE_P(Instance,
                         CftAndBftSupermajorityCheckerTest,
                         ::testing::Values(ConsistencyModel::kCft,
-                                          ConsistencyModel::kBft));
+                                          ConsistencyModel::kBft),
+                        // empty argument for the macro
+                        );
 
 INSTANTIATE_TEST_CASE_P(Instance,
                         BftSupermajorityCheckerTest,
-                        ::testing::Values(ConsistencyModel::kBft));
+                        ::testing::Values(ConsistencyModel::kBft),
+                        // empty argument for the macro
+                        );
 
 /**
  * @given 2 participants
