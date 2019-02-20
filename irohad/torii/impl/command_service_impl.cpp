b157b7908e33b186f6e660da2bbe55be716a474a
Igor Egorov <igor@soramitsu.co.jp>
2019-02-20 12:50:27 +0300

    Reduce the list of final transaction statuses (#2106)
    Make StatefulFailed and MstExpired statuses non-final
    
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- irohad/torii/impl/command_service_impl.cpp
+++ irohad/torii/impl/command_service_impl.cpp
@@ -91,14 +91,25 @@ namespace iroha {
      * Statuses considered final for streaming. Observable stops value emission
      * after receiving a value of one of the following types
      * @tparam T concrete response type
+     *
+     * StatefulFailedTxResponse and MstExpiredResponse were removed from the
+     * list of final statuses.
+     *
+     * StatefulFailedTxResponse is not a final status because the node might be
+     * in non-synchronized state and the transaction may be stateful valid from
+     * the viewpoint of up to date nodes.
+     *
+     * MstExpiredResponse is not a final status in general case because it will
+     * depend on MST expiration timeout. The transaction might expire in MST,
+     * but remain valid in terms of Iroha validation rules. Thus, it may be
+     * resent and committed successfully. As the result the final status may
+     * differ from MstExpiredResponse.
      */
     template <typename T>
     constexpr bool FinalStatusValue =
         iroha::is_any<std::decay_t<T>,
                       shared_model::interface::StatelessFailedTxResponse,
-                      shared_model::interface::StatefulFailedTxResponse,
                       shared_model::interface::CommittedTxResponse,
-                      shared_model::interface::MstExpiredResponse,
                       shared_model::interface::RejectedTxResponse>::value;
 
     rxcpp::observable<
