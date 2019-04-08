f9d19f527c60a6cb098f21c31b972b54d0d5cd0f
Alexey Rodionov <rodionov@soramitsu.co.jp>
2019-04-08 09:19:28 +0300

    Fix mac build agents (#2214)
    Signed-off-by: Alexey Rodionov <rodionov@soramitsu.co.jp>

--- .jenkinsci-new/builders/x64-mac-build-steps.groovy
+++ .jenkinsci-new/builders/x64-mac-build-steps.groovy
@@ -16,6 +16,7 @@ def testSteps(scmVars, String buildDir, List environment, String testList) {
         mkdir -p /var/jenkins/${scmVars.GIT_COMMIT}-${BUILD_NUMBER}; \
         initdb -D /var/jenkins/${scmVars.GIT_COMMIT}-${BUILD_NUMBER}/ -U ${IROHA_POSTGRES_USER} --pwfile=<(echo ${IROHA_POSTGRES_PASSWORD}); \
         pg_ctl -D /var/jenkins/${scmVars.GIT_COMMIT}-${BUILD_NUMBER}/ -o '-p 5433 -c max_prepared_transactions=100' -l /var/jenkins/${scmVars.GIT_COMMIT}-${BUILD_NUMBER}/events.log start; \
+        ./docker/release/wait-for-it.sh -h localhost -p 5433 -t 30 -- true; \
         psql -h localhost -d postgres -p 5433 -U ${IROHA_POSTGRES_USER} --file=<(echo create database ${IROHA_POSTGRES_USER};)
       """
       sh "cd build; IROHA_POSTGRES_HOST=localhost IROHA_POSTGRES_PORT=5433 ctest --output-on-failure --no-compress-output --tests-regex '${testList}'  --test-action Test || true"
@@ -60,7 +61,8 @@ def buildSteps(int parallelism, List compilerVersions, String build_type, boolea
         -DCOVERAGE=${cmakeBooleanOption[coverage]} \
         -DTESTING=${cmakeBooleanOption[testing]} \
         -DPACKAGE_TGZ=${cmakeBooleanOption[packagebuild]} \
-        -DUSE_BTF=${cmakeBooleanOption[useBTF]} ")
+        -DUSE_BTF=${cmakeBooleanOption[useBTF]} \
+        -DLIB_SUFFIX=64 ")
 
         build.cmakeBuild(buildDir, cmakeBuildOptions, parallelism)
       }
